9cc0476b58e6c4a7e975965bb4bf1ea3b1e677e3
Stef Coenen <stef@thebrownfox.be>
2023-01-03 10:02:41 +0100

    Added temp PurgeAllFiles on driveProvider;

--- src/provider/core/DriveData/DriveProvider.ts
+++ src/provider/core/DriveData/DriveProvider.ts
@@ -441,6 +441,80 @@ export class DriveProvider extends ProviderBase {
       });
   }
 
+  // This is a temporary method, and should only be used as long as there is no way to fully remove all files on a drive in one go
+  async PurgeAllFiles(targetDrive: TargetDrive): Promise<boolean> {
+    assertIfDefined('TargetDrive', targetDrive);
+
+    const includeMetadataHeader = true;
+    const pageSize = 10;
+    const maxPages = 10;
+
+    const getAllFilesOnDrive = async (drive: TargetDrive) => {
+      const querySet = async (cursorState: string | undefined): Promise<QueryBatchResponse> => {
+        return await this.QueryBatch(
+          { targetDrive: drive },
+          {
+            maxRecords: pageSize,
+            includeMetadataHeader: includeMetadataHeader,
+            cursorState: cursorState,
+          }
+        );
+      };
+
+      const searchResults: DriveSearchResult[] = [];
+      let cursorState: string | undefined = undefined;
+
+      for (let i = 0; i < maxPages; i++) {
+        const response: QueryBatchResponse = await querySet(cursorState);
+        searchResults.push(...response.searchResults);
+        cursorState = response.cursorState;
+
+        if (response.searchResults.length < pageSize) {
+          break;
+        }
+      }
+
+      return searchResults;
+    };
+
+    const searchResults = await getAllFilesOnDrive(targetDrive);
+
+    const purgeFile = (targetDrive: TargetDrive, fileId: string): Promise<boolean | void> => {
+      assertIfDefined('TargetDrive', targetDrive);
+      assertIfDefined('FileId', fileId);
+
+      const client = this.createAxiosClient();
+
+      const request = {
+        file: {
+          targetDrive: targetDrive,
+          fileId: fileId,
+        },
+        deleteLinkedFiles: true,
+      };
+
+      return client
+        .post('/drive/files/harddelete', request)
+        .then((response) => {
+          if (response.status === 200) {
+            return true;
+          }
+
+          return false;
+        })
+        .catch((error) => {
+          console.error('[DotYouCore-js]', error);
+          throw error;
+        });
+    };
+
+    await Promise.all(
+      searchResults.map(async (result) => await purgeFile(targetDrive, result.fileId))
+    );
+
+    return true;
+  }
+
   /// Upload methods:
 
   async UploadUsingKeyHeader(
