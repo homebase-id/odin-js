fdb6787fd1cbc5dc395c47b931430e0d1bd6a2ef
Stef Coenen <stef@thebrownfox.be>
2023-04-14 08:44:14 +0200

    Added batchSize to processInbox;

--- src/provider/core/TransitData/InboxProvider.ts
+++ src/provider/core/TransitData/InboxProvider.ts
@@ -1,10 +1,15 @@
 import { DotYouClient } from '../DotYouClient';
 import { TargetDrive } from '../DriveData/DriveTypes';
 
-export const processInbox = async (dotYouClient: DotYouClient, targetDrive: TargetDrive) => {
+export const processInbox = async (
+  dotYouClient: DotYouClient,
+  targetDrive: TargetDrive,
+  batchSize?: number
+) => {
   const client = dotYouClient.createAxiosClient();
 
   return client.post('/transit/inbox/processor/process', {
     targetDrive,
+    batchSize,
   });
 };
