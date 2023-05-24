178af6cad7c8c00b3ffb4953254da3960c972b86
Stef Coenen <stef@thebrownfox.be>
2023-05-24 12:02:41 +0200

    Added ignore for module level directives;

--- packages/owner-app/vite.config.ts
+++ packages/owner-app/vite.config.ts
@@ -26,6 +26,16 @@ export default defineConfig({
     ...hostConfig,
   },
   base: '/owner',
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
 });
 
 /**
