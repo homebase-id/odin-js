40743885f2031cde20be8553de1666786170c3ba
Stef Coenen <stef@thebrownfox.be>
2023-08-14 10:37:15 +0200

    Updated loadingBlock with more applicable pulse on light;

--- packages/ui-lib/src/components/ui/LoadingBlock/LoadingBlock.tsx
+++ packages/ui-lib/src/components/ui/LoadingBlock/LoadingBlock.tsx
@@ -1,6 +1,6 @@
 const LoadingBlock = ({ className = 'w-full h-2 m-0' }: { className?: string }) => {
   return (
-    <div className={`${className} animate-pulse rounded bg-slate-100 dark:bg-slate-700`}></div>
+    <div className={`${className} animate-pulse rounded bg-slate-200 dark:bg-slate-700`}></div>
   );
 };
 
