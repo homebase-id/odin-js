ab62e5aecf05e027fd3883eadd2c1339a86d9c25
Stef Coenen <stef@thebrownfox.be>
2023-08-28 17:18:27 +0200

    Fixed owner logout;

--- packages/common-app/src/provider/OwnerAuthenticationProvider/OwnerAuthenticationProvider.ts
+++ packages/common-app/src/provider/OwnerAuthenticationProvider/OwnerAuthenticationProvider.ts
@@ -18,8 +18,6 @@ export const logoutOwner = async (): Promise<boolean> => {
   const dotYouClient = new OwnerClient({ api: ApiType.Owner });
   const client = dotYouClient.createAxiosClient({ overrideEncryption: true });
 
-  await client.get('/auth/delete-token', { baseURL: '/api/youauth/v1' });
-
   //withCredentials lets us set the cookies return from the /admin/authentication endpoint
   return client.get('/authentication/logout', { withCredentials: true }).then((response) => {
     return response.data;
