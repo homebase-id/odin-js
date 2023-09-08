b3f39e1b5a40cd05ab8697baf03164567add6616
Stef Coenen <stef@thebrownfox.be>
2023-09-08 11:29:33 +0200

    Volatile input selection restoration (#19)
    * Updated loginbox;
    
    * WIP Improved restoration;
    
    * WIP Improved restoration;

--- packages/common-app/src/form/VolatileInput.tsx
+++ packages/common-app/src/form/VolatileInput.tsx
@@ -79,16 +79,15 @@ export const VolatileInput = ({
 
     const relativeOffset = getRelativeOffset(absoluteOffset, divRef.current);
 
-    if (relativeOffset) {
-      relativeOffset.offset += offset || 0;
+    if (!relativeOffset) return;
 
-      restoreSelection([
-        relativeOffset.node,
-        Math.max(0, relativeOffset.offset),
-        relativeOffset.node,
-        Math.max(0, relativeOffset.offset),
-      ]);
-    }
+    relativeOffset.offset += offset || 0;
+    restoreSelection([
+      relativeOffset.node,
+      Math.max(0, relativeOffset.offset),
+      relativeOffset.node,
+      Math.max(0, relativeOffset.offset),
+    ]);
   };
 
   const wrapLinks = () => {
@@ -111,19 +110,18 @@ export const VolatileInput = ({
   };
 
   useEffect(() => {
-    if (divRef?.current) {
-      if (divRef.current.innerText !== defaultValue) {
-        const caretPos = saveCaretPosition();
+    if (!divRef?.current) return;
+    if (divRef.current.innerText === defaultValue) return;
 
-        divRef.current.innerText = defaultValue || '';
+    const caretPos = saveCaretPosition();
 
-        restoreCaretPosition(
-          caretPos,
-          supportEmojiShortcut && emojiQuery ? -emojiQuery.length : undefined
-        );
-        if (supportEmojiShortcut && emojiQuery) setEmojiQuery(undefined);
-      }
-    }
+    divRef.current.innerText = defaultValue || '';
+
+    restoreCaretPosition(
+      caretPos,
+      supportEmojiShortcut && emojiQuery ? -emojiQuery.length : undefined
+    );
+    if (supportEmojiShortcut && emojiQuery) setEmojiQuery(undefined);
   }, [defaultValue]);
 
   // We want values to be saved directly, while the link styling is better with a debounce
@@ -164,7 +162,7 @@ export const VolatileInput = ({
   };
 
   const doLinkStyle = () => wrapLinks();
-  const debouncedLinkStyle = useDebounce(doLinkStyle, { timeoutMillis: 500 });
+  const debouncedLinkStyle = useDebounce(doLinkStyle, { timeoutMillis: 1000 });
 
   const selection = supportEmojiShortcut && window.getSelection(),
     range = selection && selection.rangeCount ? selection.getRangeAt(0) : undefined,
