2a4e4ae78b9be2dd17e11e33cf5164bb6f4f3b5d
Stef Coenen <stef@thebrownfox.be>
2023-09-26 10:01:56 +0200

    Simplified cover;

--- packages/public-app/src/templates/Home/Cover/HomeCover.tsx
+++ packages/public-app/src/templates/Home/Cover/HomeCover.tsx
@@ -19,8 +19,6 @@ const HomeCover = ({ templateSettings }: { templateSettings?: ThemeCoverSettings
   const { owner } = useSiteData().data ?? {};
   const { isAuthenticated, isOwner } = useAuth();
   const { data: channels } = useChannels({ isAuthenticated, isOwner });
-  const { data: connections } = useActiveConnections({ pageSize: 10 }).fetch;
-
   if (!owner) return null;
 
   const targetDrive = {
@@ -29,64 +27,43 @@ const HomeCover = ({ templateSettings }: { templateSettings?: ThemeCoverSettings
   };
 
   return (
-    <>
-      <section className="body-font my-auto">
-        <div className="container mx-auto">
-          <div className="lg:flex">
-            <div className="mb-12 lg:w-1/2">
-              {owner?.profileImageId && targetDrive ? (
-                <Image
-                  fileId={owner?.profileImageId}
-                  targetDrive={targetDrive}
-                  className="w-full max-w-[none] sm:mx-auto md:max-w-[30rem] lg:max-w-[none]"
-                />
-              ) : null}
+    <section className="body-font my-auto">
+      <div className="container mx-auto px-5">
+        <div className="flex flex-col lg:flex-row lg:gap-5">
+          <div className="mx-auto mb-12 max-w-[none] md:max-w-[30rem] lg:w-1/2 lg:max-w-[35rem]">
+            {owner?.profileImageId && targetDrive ? (
+              <Image fileId={owner?.profileImageId} targetDrive={targetDrive} className="w-full" />
+            ) : null}
+          </div>
+          <div className="flex h-full flex-grow flex-col lg:w-1/2">
+            <h1 className="mb-4 text-2xl">
+              <OwnerName />
+              <br />
+              <small>{templateSettings?.tagLine ?? ''}</small>
+            </h1>
+            {templateSettings?.leadText ? (
+              <RichTextRenderer body={templateSettings?.leadText} />
+            ) : null}
+            <div className="mt-4 flex flex-row">
+              <FollowLink className="my-1 mr-3" />
+              <ConnectLink className="my-1" />
             </div>
-            <div className="flex h-full flex-col px-5 lg:w-1/2">
-              <h1 className="mb-4 text-2xl">
-                <OwnerName />
-                <br />
-                <small>{templateSettings?.tagLine ?? ''}</small>
-              </h1>
-              {templateSettings?.leadText ? (
-                <RichTextRenderer body={templateSettings?.leadText} />
-              ) : null}
-              <div className="mt-4 flex flex-row">
-                <FollowLink className="my-1 mr-3" />
-                <ConnectLink className="my-1" />
-              </div>
-              <div className="-mb-4 mt-auto py-12">
-                {channels?.map((channel) => {
-                  return (
-                    <ChannelTeaser
-                      key={channel.channelId}
-                      channel={channel}
-                      className={'w-full py-4'}
-                    />
-                  );
-                })}
-                <Links className="justify-center" style="secondary" direction="row" />
-              </div>
+            <div className="-mb-4 mt-auto py-12">
+              {channels?.map((channel) => {
+                return (
+                  <ChannelTeaser
+                    key={channel.channelId}
+                    channel={channel}
+                    className={'w-full py-4'}
+                  />
+                );
+              })}
+              <Links className="justify-center" style="secondary" direction="row" />
             </div>
           </div>
-          {connections?.pages[0]?.results.length ? (
-            <div className="mb-20 mt-16 px-5">
-              <div className="-m-3 flex flex-wrap">
-                {connections.pages[0]?.results?.map((item, index) => {
-                  return (
-                    <IdentityTeaser
-                      key={index}
-                      odinId={item?.odinId}
-                      className="p-2 md:w-1/2 lg:w-1/3"
-                    />
-                  );
-                })}
-              </div>
-            </div>
-          ) : null}
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
