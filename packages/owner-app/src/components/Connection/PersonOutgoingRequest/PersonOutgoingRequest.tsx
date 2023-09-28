868f4f15db54870d116f3b53be69be3da8d48078
Stef Coenen <stef@thebrownfox.be>
2023-09-28 17:33:35 +0200

    Some more stopPropogations;

--- packages/owner-app/src/components/Connection/PersonOutgoingRequest/PersonOutgoingRequest.tsx
+++ packages/owner-app/src/components/Connection/PersonOutgoingRequest/PersonOutgoingRequest.tsx
@@ -34,6 +34,7 @@ const PersonOutgoingRequest = ({
           className="mb-2 w-full"
           onClick={(e) => {
             e.preventDefault();
+            e.stopPropagation();
             revokeRequest({ targetOdinId: recipientOdinId });
             return false;
           }}
