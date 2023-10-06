7c3921c365461e834c3ae1104680a200f3a1a17a
Stef Coenen <stef@thebrownfox.be>
2023-10-06 10:32:55 +0200

    Improved error handling of the login box value;

--- packages/login-app/src/logon.ts
+++ packages/login-app/src/logon.ts
@@ -25,6 +25,8 @@ const setupHtml = () => {
 };
 
 const domainRegex = /^(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9]{2,25}(?::\d{1,5})?$/i;
+const INVALID_CLASSNAME = 'invalid';
+const LOADING_CLASSNAME = 'loading';
 
 export const setupLogon = async () => {
   setupHtml();
@@ -42,6 +44,31 @@ export const setupLogon = async () => {
 
   if (!mainForm || !dotyouInputBox) return;
 
+  const localDomainComplete = (domain: string) => {
+    const strippedIdentity = stripIdentity(domain);
+    if (strippedIdentity.split('.').length >= 2) return true;
+
+    mainForm.classList.remove(INVALID_CLASSNAME);
+    return false;
+  };
+
+  const localDomainCheck = (domain: string) => {
+    const strippedIdentity = stripIdentity(domain);
+    if (domainRegex.test(strippedIdentity)) {
+      mainForm.classList.remove(INVALID_CLASSNAME);
+      return true;
+    }
+    mainForm.classList.add(INVALID_CLASSNAME);
+    return false;
+  };
+
+  const debouncedDomainValidator = debounce(async (e) => {
+    if (!localDomainComplete(e.target.value)) return;
+    mainForm.classList.toggle(INVALID_CLASSNAME, !localDomainCheck(e.target.value));
+  }, 500);
+
+  dotyouInputBox.addEventListener('keydown', debouncedDomainValidator);
+
   const pingIdentity = async (identity: string) => {
     return await fetch(`https://${identity}/api/guest/v1/auth/ident`)
       .then((response) => response.json())
@@ -49,54 +76,39 @@ export const setupLogon = async () => {
       .catch(() => false);
   };
 
-  const debouncedDomainValidator = debounce(async (e) => {
-    const strippedIdentity = stripIdentity(e.target.value);
-    const isComplete = strippedIdentity.split('.').length >= 2;
-
-    if (!isComplete) return mainForm.classList.remove('invalid');
-
-    const isValid = domainRegex.test(strippedIdentity);
-
-    mainForm.classList.toggle('invalid', !isValid);
-  }, 500);
-
-  dotyouInputBox.addEventListener('keydown', debouncedDomainValidator);
-
-  const fillIdentityFromStorage = () => {
-    const previousIdentity = getIdentityFromStorage();
-    if (!dotyouInputBox.value && previousIdentity) dotyouInputBox.value = previousIdentity;
-  };
-
   const storagePartioned = await checkStorageAccess();
   mainForm.addEventListener('submit', async (e) => {
     e.preventDefault();
 
-    if (mainForm.classList.contains('invalid')) return;
-    mainForm.classList.add('loading');
+    if (!localDomainCheck(dotyouInputBox.value)) return;
+    mainForm.classList.add(LOADING_CLASSNAME);
 
     const strippedIdentity = stripIdentity(dotyouInputBox.value);
     if (!(await pingIdentity(strippedIdentity))) {
-      mainForm.classList.add('invalid');
-      mainForm.classList.remove('loading');
+      mainForm.classList.add(INVALID_CLASSNAME);
+      mainForm.classList.remove(LOADING_CLASSNAME);
       return;
     }
 
-    // If storage is partioned, onclick of the input box, requestAccess to store the identity (We need a user interaction before we can request access)
+    // If storage is partioned, try and request access to store the identity
     if (storagePartioned) await requestStorageAccess();
     storeIdentityAndAuthorize(strippedIdentity, urlParams);
 
     return false;
   });
 
+  const fillIdentityFromStorage = () => {
+    const previousIdentity = getIdentityFromStorage();
+    if (!dotyouInputBox.value && previousIdentity) dotyouInputBox.value = previousIdentity;
+  };
+
   // If storage is partioned, onclick of the input box, requestAccess to fill in with a previous known identity
   if (storagePartioned) {
     dotyouInputBox.addEventListener('click', async (e) => {
       if (!e.target || 'value' in e.target) return;
-
       if ((e.target as HTMLInputElement).value) return;
-      requestStorageAccess().then(() => {
-        fillIdentityFromStorage();
-      });
+
+      requestStorageAccess().then(() => fillIdentityFromStorage());
     });
   } else fillIdentityFromStorage();
 };
