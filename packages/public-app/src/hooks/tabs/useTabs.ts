53c36444d22573138be1a4084a44e4b142704fcf
Stef Coenen <stef@thebrownfox.be>
2023-10-06 14:35:07 +0200

    Removed check of isAuthenticated to show connections tab;

--- packages/public-app/src/hooks/tabs/useTabs.ts
+++ packages/public-app/src/hooks/tabs/useTabs.ts
@@ -1,21 +1,15 @@
 import { t } from '@youfoundation/common-app';
-import useAuth from '../auth/useAuth';
 import useTheme from '../theme/useTheme';
 
 const useTabs = () => {
-  const { isAuthenticated } = useAuth();
   const { isTabs, tabsOrder } = useTheme();
 
-  let tabsArray = tabsOrder.map((tab) => {
+  const tabsArray = tabsOrder.map((tab) => {
     return {
       key: tab.toLowerCase(),
       title: t(tab),
     };
   });
-
-  if (!isAuthenticated) {
-    tabsArray = tabsArray.filter((tab) => tab.key !== 'connections');
-  }
 
   return {
     isTabs,
