ed2938dc8a36f8fa0f8e131b4d1d267e0ce4a72d
Stef Coenen <stef@thebrownfox.be>
2023-10-09 09:50:04 +0200

    Remove non-custom favicons from default set of meta;

--- packages/public-app/src/components/ui/Layout/Layout.tsx
+++ packages/public-app/src/components/ui/Layout/Layout.tsx
@@ -65,7 +65,6 @@ export const NoLayout: FC<LayoutProps> = ({ children }) => {
       <Helmet>
         {imageData || emojiFavicon ? (
           <>
-            {/* <meta name="favicon" content={faviconId} /> */}
             <link rel="shortcut icon" href={imageData?.url || emojiFavicon}></link>
           </>
         ) : (
@@ -73,6 +72,7 @@ export const NoLayout: FC<LayoutProps> = ({ children }) => {
             <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
             <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
             <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+            <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
           </>
         )}
       </Helmet>
