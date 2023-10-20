684620c6e02eb4f68200ffe74cc7fee357ffea26
Stef Coenen <stef@thebrownfox.be>
2023-10-20 12:16:57 +0200

    Added file queries by uniqueId into the js-lib;

--- packages/js-lib/src/core/DriveData/DriveFileProvider.ts
+++ packages/js-lib/src/core/DriveData/DriveFileProvider.ts
@@ -25,7 +25,7 @@ interface GetFileRequest {
 
 const _internalMetadataPromiseCache = new Map<string, Promise<DriveSearchResult | null>>();
 
-/// Get methods:
+/// Get methods by FileId:
 export const getFileHeader = async (
   dotYouClient: DotYouClient,
   targetDrive: TargetDrive,
@@ -35,17 +35,13 @@ export const getFileHeader = async (
   assertIfDefined('TargetDrive', targetDrive);
   assertIfDefined('FileId', fileId);
 
-  const cacheKey = `${targetDrive.alias}-${targetDrive.type}+${fileId}`;
-  if (_internalMetadataPromiseCache.has(cacheKey)) {
-    const cacheEntry = await _internalMetadataPromiseCache.get(cacheKey);
-    if (cacheEntry) return cacheEntry;
-  }
+  const cacheKey = getCacheKey(targetDrive, fileId);
+  const cacheEntry =
+    _internalMetadataPromiseCache.has(cacheKey) &&
+    (await _internalMetadataPromiseCache.get(cacheKey));
+  if (cacheEntry) return cacheEntry;
 
-  const client = dotYouClient.createAxiosClient({
-    headers: {
-      'X-ODIN-FILE-SYSTEM-TYPE': systemFileType || 'Standard',
-    },
-  });
+  const client = getAxiosClient(dotYouClient, systemFileType);
 
   const request: GetFileRequest = {
     ...targetDrive,
@@ -77,27 +73,7 @@ export const getPayloadAsJson = async <T>(
   systemFileType?: SystemFileType
 ): Promise<T | null> => {
   return getPayloadBytes(dotYouClient, targetDrive, fileId, keyHeader, systemFileType).then(
-    (data) => {
-      if (!data) return null;
-      const json = byteArrayToString(new Uint8Array(data.bytes));
-      try {
-        const o = JSON.parse(json);
-        return o;
-      } catch (ex) {
-        console.warn('base JSON.parse failed');
-        const replaceAll = (str: string, find: string, replace: string) => {
-          return str.replace(new RegExp(find, 'g'), replace);
-        };
-
-        const jsonWithRemovedQuote = replaceAll(json, '\u0019', '');
-        const jsonWithRemovedEmDash = replaceAll(jsonWithRemovedQuote, '\u0014', '');
-
-        const o = JSON.parse(jsonWithRemovedEmDash);
-
-        console.warn('... but we fixed it');
-        return o;
-      }
-    }
+    parseBytesToObject
   );
 };
 
@@ -113,11 +89,7 @@ export const getPayloadBytes = async (
   assertIfDefined('TargetDrive', targetDrive);
   assertIfDefined('FileId', fileId);
 
-  const client = dotYouClient.createAxiosClient({
-    headers: {
-      'X-ODIN-FILE-SYSTEM-TYPE': systemFileType || 'Standard',
-    },
-  });
+  const client = getAxiosClient(dotYouClient, systemFileType);
   const request: GetFileRequest = {
     ...targetDrive,
     fileId,
@@ -127,19 +99,11 @@ export const getPayloadBytes = async (
     responseType: 'arraybuffer',
   };
 
-  let startOffset = 0;
-  let updatedChunkEnd: number | undefined, updatedChunkStart: number | undefined;
-  if (chunkStart !== undefined) {
-    updatedChunkStart = chunkStart === 0 ? 0 : roundToSmallerMultipleOf16(chunkStart - 16);
-    startOffset = Math.abs(chunkStart - updatedChunkStart);
-    updatedChunkEnd = chunkEnd !== undefined ? roundToLargerMultipleOf16(chunkEnd) : undefined;
-
-    config.headers = {
-      ...config.headers,
-      range: `bytes=${updatedChunkStart}-${updatedChunkEnd || ''}`,
-    };
-  }
-
+  const { startOffset, updatedChunkStart, rangeHeader } = getRangeHeader(chunkStart, chunkEnd);
+  config.headers = {
+    ...config.headers,
+    range: rangeHeader,
+  };
   return client
     .get<ArrayBuffer>('/drive/files/payload?' + stringifyToQueryParams(request as any), config)
     .then(async (response) => {
@@ -186,11 +150,7 @@ export const getThumbBytes = async (
   assertIfDefined('Width', width);
   assertIfDefined('Height', height);
 
-  const client = dotYouClient.createAxiosClient({
-    headers: {
-      'X-ODIN-FILE-SYSTEM-TYPE': systemFileType || 'Standard',
-    },
-  });
+  const client = getAxiosClient(dotYouClient, systemFileType);
   const request: GetFileRequest = {
     ...targetDrive,
     fileId,
@@ -218,7 +178,228 @@ export const getThumbBytes = async (
     });
 };
 
+interface GetFileByUniqueIdRequest {
+  alias: string;
+  type: string;
+  clientUniqueId: string;
+}
+
+/// Get methods by UniqueId
+export const getFileHeaderByUniqueId = async (
+  dotYouClient: DotYouClient,
+  targetDrive: TargetDrive,
+  uniqueId: string,
+  systemFileType?: SystemFileType
+): Promise<DriveSearchResult | null> => {
+  assertIfDefined('TargetDrive', targetDrive);
+  assertIfDefined('UniqueId', uniqueId);
+
+  const cacheKey = getCacheKey(targetDrive, uniqueId);
+  const cacheEntry =
+    _internalMetadataPromiseCache.has(cacheKey) &&
+    (await _internalMetadataPromiseCache.get(cacheKey));
+  if (cacheEntry) return cacheEntry;
+
+  const client = getAxiosClient(dotYouClient, systemFileType);
+
+  const request: GetFileByUniqueIdRequest = {
+    ...targetDrive,
+    clientUniqueId: uniqueId,
+  };
+
+  const promise: Promise<DriveSearchResult | null> = client
+    .get('/drive/query/specialized/cuid/header?' + stringifyToQueryParams(request as any))
+    .then((response) => {
+      _internalMetadataPromiseCache.delete(cacheKey);
+      return response.data;
+    })
+    .catch((error) => {
+      if (error.response?.status === 404) return null;
+      console.error('[DotYouCore-js:getFileHeaderByUniqueId]', error);
+      throw error;
+    });
+
+  _internalMetadataPromiseCache.set(cacheKey, promise);
+
+  return promise;
+};
+
+export const getPayloadAsJsonByUniqueId = async <T>(
+  dotYouClient: DotYouClient,
+  targetDrive: TargetDrive,
+  uniqueId: string,
+  keyHeader: KeyHeader | EncryptedKeyHeader | undefined,
+  systemFileType?: SystemFileType
+): Promise<T | null> => {
+  return getPayloadBytesByUniqueId(
+    dotYouClient,
+    targetDrive,
+    uniqueId,
+    keyHeader,
+    systemFileType
+  ).then(parseBytesToObject);
+};
+
+export const getPayloadBytesByUniqueId = async (
+  dotYouClient: DotYouClient,
+  targetDrive: TargetDrive,
+  uniqueId: string,
+  keyHeader: KeyHeader | EncryptedKeyHeader | undefined,
+  systemFileType?: SystemFileType,
+  chunkStart?: number,
+  chunkEnd?: number
+): Promise<{ bytes: Uint8Array; contentType: ImageContentType } | null> => {
+  assertIfDefined('TargetDrive', targetDrive);
+  assertIfDefined('UniqueId', uniqueId);
+
+  const client = getAxiosClient(dotYouClient, systemFileType);
+  const request: GetFileByUniqueIdRequest = {
+    ...targetDrive,
+    clientUniqueId: uniqueId,
+  };
+
+  const config: AxiosRequestConfig = {
+    responseType: 'arraybuffer',
+  };
+
+  const { startOffset, updatedChunkStart, rangeHeader } = getRangeHeader(chunkStart, chunkEnd);
+  config.headers = {
+    ...config.headers,
+    range: rangeHeader,
+  };
+
+  return client
+    .get<ArrayBuffer>(
+      '/drive/query/specialized/cuid/payload?' + stringifyToQueryParams(request as any),
+      config
+    )
+    .then(async (response) => {
+      if (!response.data) return null;
+      return {
+        bytes:
+          updatedChunkStart !== undefined
+            ? (
+                await decryptChunkedBytesResponse(
+                  dotYouClient,
+                  response,
+                  startOffset,
+                  updatedChunkStart
+                )
+              ).slice(
+                0,
+                chunkEnd !== undefined && chunkStart !== undefined
+                  ? chunkEnd - chunkStart + 1
+                  : undefined
+              )
+            : await decryptBytesResponse(dotYouClient, response, keyHeader),
+
+        contentType: `${response.headers.decryptedcontenttype}` as ImageContentType,
+      };
+    })
+    .catch((error) => {
+      if (error.response?.status === 404) return null;
+      console.error('[DotYouCore-js:getPayloadBytes]', error);
+      return null;
+    });
+};
+
+export const getThumbBytesByUniqueId = async (
+  dotYouClient: DotYouClient,
+  targetDrive: TargetDrive,
+  uniqueId: string,
+  keyHeader: KeyHeader | undefined,
+  width: number,
+  height: number,
+  systemFileType?: SystemFileType
+): Promise<{ bytes: ArrayBuffer; contentType: ImageContentType } | null> => {
+  assertIfDefined('TargetDrive', targetDrive);
+  assertIfDefined('UniqueId', uniqueId);
+  assertIfDefined('Width', width);
+  assertIfDefined('Height', height);
+
+  const client = getAxiosClient(dotYouClient, systemFileType);
+  const request: GetFileByUniqueIdRequest = {
+    ...targetDrive,
+    clientUniqueId: uniqueId,
+  };
+  const config: AxiosRequestConfig = {
+    responseType: 'arraybuffer',
+  };
+
+  return client
+    .get<ArrayBuffer>(
+      '/drive/files/thumb?' + stringifyToQueryParams({ ...request, width, height }),
+      config
+    )
+    .then(async (response) => {
+      if (!response.data) return null;
+      return {
+        bytes: await decryptBytesResponse(dotYouClient, response, keyHeader),
+        contentType: `${response.headers.decryptedcontenttype}` as ImageContentType,
+      };
+    })
+    .catch((error) => {
+      if (error.response?.status === 404) return null;
+      console.error('[DotYouCore-js:getThumbBytes]', error);
+      return null;
+    });
+};
+
 /// Helper methods:
+const getCacheKey = (targetDrive: TargetDrive, id: string) =>
+  `${targetDrive.alias}-${targetDrive.type}+${id}`;
+
+const getAxiosClient = (dotYouClient: DotYouClient, systemFileType?: SystemFileType) =>
+  dotYouClient.createAxiosClient({
+    headers: {
+      'X-ODIN-FILE-SYSTEM-TYPE': systemFileType || 'Standard',
+    },
+  });
+
+const parseBytesToObject = (
+  data: {
+    bytes: Uint8Array;
+    contentType: ImageContentType;
+  } | null
+) => {
+  if (!data) return null;
+  const json = byteArrayToString(new Uint8Array(data.bytes));
+  try {
+    const o = JSON.parse(json);
+    return o;
+  } catch (ex) {
+    console.warn('base JSON.parse failed');
+    const replaceAll = (str: string, find: string, replace: string) => {
+      return str.replace(new RegExp(find, 'g'), replace);
+    };
+
+    const jsonWithRemovedQuote = replaceAll(json, '\u0019', '');
+    const jsonWithRemovedEmDash = replaceAll(jsonWithRemovedQuote, '\u0014', '');
+
+    const o = JSON.parse(jsonWithRemovedEmDash);
+
+    console.warn('... but we fixed it');
+    return o;
+  }
+};
+
+const getRangeHeader = (chunkStart?: number, chunkEnd?: number) => {
+  let startOffset = 0;
+  let updatedChunkEnd: number | undefined, updatedChunkStart: number | undefined;
+  if (chunkStart !== undefined) {
+    updatedChunkStart = chunkStart === 0 ? 0 : roundToSmallerMultipleOf16(chunkStart - 16);
+    startOffset = Math.abs(chunkStart - updatedChunkStart);
+    updatedChunkEnd = chunkEnd !== undefined ? roundToLargerMultipleOf16(chunkEnd) : undefined;
+
+    return {
+      startOffset,
+      updatedChunkStart,
+      rangeHeader: `bytes=${updatedChunkStart}-${updatedChunkEnd || ''}`,
+    };
+  }
+  return { startOffset, updatedChunkStart, rangeHeader: undefined };
+};
+
 // TODO: Rename this one getContentFromHeaderOrPayload
 export const getPayload = async <T>(
   dotYouClient: DotYouClient,
