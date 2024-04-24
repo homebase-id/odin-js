e3a86a5fee3c90329aeee499c6f536f72655d75e
Stef Coenen <stef@thebrownfox.be>
2024-04-24 08:44:30 +0200

    Cleanup comment;

--- packages/mail-app/src/components/Sidenav/MailSidenav.tsx
+++ packages/mail-app/src/components/Sidenav/MailSidenav.tsx
@@ -21,7 +21,6 @@ const ROOT_PATH = '/apps/mail';
 export const MailSidenav = () => {
   const { logout } = useAuth();
 
-  // TODO: Check if we should move it up the render tree to have common coverage of detail views
   const isOnline = useLiveMailProcessor();
   useRemoveNotifications({ appId: MAIL_APP_ID });
 
