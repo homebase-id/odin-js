172bc3146e1a0b46d5a1a05c49b061701cce1c6d
Bishwajeet Parhi <62933155+2002Bishwajeet@users.noreply.github.com>
2024-04-30 15:57:36 +0530

    fix: Can't download payload due to overlay dismissing the screen (#106)
    * fix: Can't download payload due to overlay dismissing the screen
    
    * Made the preview still hide when clicked behind;
    
    ---------
    
    Co-authored-by: Stef Coenen <stef@thebrownfox.be>

--- packages/mail-app/src/components/Thread/MailAttachmentPreview.tsx
+++ packages/mail-app/src/components/Thread/MailAttachmentPreview.tsx
@@ -91,9 +91,6 @@ export const MailAttachmentPreview = ({
     };
   }, [mailMessage, doSlide]);
 
-  const wrapperRef = useRef<HTMLDivElement>(null);
-  useOutsideTrigger(wrapperRef, () => doClose());
-
   const getFileUrl = useMailAttachment().fetchAttachment;
   const doDownload = async () => {
     const url = await getFileUrl(messageId, payloadKey, payloadDescriptor?.contentType as string);
@@ -110,9 +107,13 @@ export const MailAttachmentPreview = ({
 
   return (
     <div
+      onClick={doClose}
       className={`fixed inset-0 z-50 flex flex-col overflow-auto bg-slate-900 bg-opacity-90 backdrop-blur-sm lg:overflow-hidden`}
     >
-      <div className="flex w-full flex-row flex-wrap bg-slate-950 px-3 py-3 text-white">
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="flex w-full flex-row flex-wrap bg-slate-950 px-3 py-3 text-white"
+      >
         {payloadDescriptor ? (
           <>
             <div className="flex flex-row items-center gap-2">
@@ -146,8 +147,8 @@ export const MailAttachmentPreview = ({
         ) : null}
       </div>
       <div
+        onClick={(e) => e.stopPropagation()}
         className="mx-auto my-auto flex w-full max-w-3xl flex-col items-center justify-center"
-        ref={wrapperRef}
       >
         {mailMessageLoading || !payloadDescriptor ? (
           <Loader className="h-20 w-20" />
