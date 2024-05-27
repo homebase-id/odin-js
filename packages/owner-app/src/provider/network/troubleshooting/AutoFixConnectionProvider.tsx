4d842fafce408cf14594d7131f830d4564899a12
Stef Coenen <stef@thebrownfox.be>
2024-05-27 11:50:23 +0200

    Added auto fix connections call; (#136)

--- packages/owner-app/src/provider/network/troubleshooting/AutoFixConnectionProvider.tsx
+++ packages/owner-app/src/provider/network/troubleshooting/AutoFixConnectionProvider.tsx
@@ -2,12 +2,9 @@ import { DotYouClient } from '@youfoundation/js-lib/core';
 import { assertIfDefined } from '@youfoundation/js-lib/helpers';
 
 const autoFixPath = '/circles/connections/autofix';
-export const runAutoFix = async (dotYouClient: DotYouClient) => {
+export const autoFixConnections = async (dotYouClient: DotYouClient) => {
   assertIfDefined('DotYouClient is required', dotYouClient);
 
   const client = dotYouClient.createAxiosClient();
-  return await client
-      .post<{}>(autoFixPath, { })
-      .then((res) => res.data)
-      .catch(dotYouClient.handleErrorResponse);
+  return await client.post<unknown>(autoFixPath, {}).catch(dotYouClient.handleErrorResponse);
 };
