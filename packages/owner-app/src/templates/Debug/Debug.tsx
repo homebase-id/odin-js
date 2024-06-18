91b0cab26d91880584dfa2fdc0a9a630382f3d75
Stef Coenen <stef@thebrownfox.be>
2024-06-18 17:10:00 +0200

    Better debug;

--- packages/owner-app/src/templates/Debug/Debug.tsx
+++ packages/owner-app/src/templates/Debug/Debug.tsx
@@ -1,28 +1,47 @@
-import {
-  Label,
-  Input,
-  Select,
-  ActionButton,
-  Times,
-  t,
-  Plus,
-  DictionaryEditor,
-} from '@youfoundation/common-app';
+import { Label, Input, Select, ActionButton, Times, t, Textarea } from '@youfoundation/common-app';
 import { useAuth } from '../../hooks/auth/useAuth';
-import { useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { PageMeta } from '../../components/ui/PageMeta/PageMeta';
-import { hasDebugFlag, stringifyToQueryParams } from '@youfoundation/js-lib/helpers';
+import {
+  hasDebugFlag,
+  jsonStringify64,
+  stringifyToQueryParams,
+  tryJsonParse,
+} from '@youfoundation/js-lib/helpers';
+
+const STORAGE_KEY = 'debug-draft';
+interface DefaulValue {
+  url: string;
+  isPost: boolean;
+  headers?: Record<string, string>;
+  params?: Record<string, string>;
+}
 
 const Debug = () => {
   const dotYouClient = useAuth().getDotYouClient();
-  const [url, setUrl] = useState<string>('/circles/definitions/list');
-  const [isPost, setIsPost] = useState<boolean>(false);
 
-  const [headers, setHeaders] = useState<Record<string, string> | undefined>(undefined);
-  const [params, setParams] = useState<Record<string, string> | undefined>(undefined);
+  const defaultValues = tryJsonParse<DefaulValue>(window.localStorage.getItem(STORAGE_KEY) || '');
+
+  const [url, setUrl] = useState<string>(defaultValues.url || '/circles/definitions/list');
+  const [isPost, setIsPost] = useState<boolean>(defaultValues.isPost);
+
+  const [headers, setHeaders] = useState<Record<string, string> | undefined>(defaultValues.headers);
+  const [params, setParams] = useState<Record<string, string> | undefined>(defaultValues.params);
 
   const [outputText, setOutputText] = useState<string>('');
   const addToOutput = (text: string) => setOutputText((oldVal) => oldVal + text);
+
+  useEffect(() => {
+    window.localStorage.setItem(
+      STORAGE_KEY,
+      jsonStringify64({
+        url,
+        isPost,
+        headers,
+        params,
+      })
+    );
+  }, [url, isPost, headers, params]);
 
   const doRequest = async () => {
     const client = dotYouClient.createAxiosClient({ headers });
@@ -40,6 +59,17 @@ const Debug = () => {
 
   const isDebug = hasDebugFlag();
   if (!isDebug) return null;
+
+  const defaultHeaders = headers
+    ? Object.keys(headers)
+        .map((key) => `${key}: ${headers[key]}`)
+        .join('\n')
+    : '';
+  const defaultParams = params
+    ? Object.keys(params)
+        .map((key) => `${key}: ${params[key]}`)
+        .join('\n')
+    : '';
 
   return (
     <>
@@ -63,8 +93,30 @@ const Debug = () => {
           </div>
         </div>
 
-        <DictionaryEditor title="Headers" defaultValue={headers || {}} onChange={setHeaders} />
-        <DictionaryEditor title="Params" defaultValue={params || {}} onChange={setParams} />
+        <div>
+          <Label>{t('Headers')}</Label>
+          <Textarea
+            title="Headers"
+            defaultValue={defaultHeaders}
+            placeholder="key: value"
+            onChange={(e) => {
+              const val = e.currentTarget.value;
+              setHeaders(getDictionaryFromText(val));
+            }}
+          />
+        </div>
+        <div>
+          <Label>{t('Data')}</Label>
+          <Textarea
+            title="Params"
+            defaultValue={defaultParams}
+            placeholder="key: value"
+            onChange={(e) => {
+              const val = e.currentTarget.value;
+              setParams(getDictionaryFromText(val));
+            }}
+          />
+        </div>
 
         <div className="flex flex-row-reverse justify-between">
           <ActionButton onClick={doRequest}>Send</ActionButton>
@@ -78,6 +130,17 @@ const Debug = () => {
         <textarea value={outputText} readOnly className="min-h-[20rem] w-full flex-grow" />
       </div>
     </>
+  );
+};
+
+const getDictionaryFromText = (text: string) => {
+  return text.split('\n').reduce(
+    (acc, line) => {
+      const [key, value] = line.split(':');
+      acc[key.trim()] = value.trim();
+      return acc;
+    },
+    {} as Record<string, string>
   );
 };
 
