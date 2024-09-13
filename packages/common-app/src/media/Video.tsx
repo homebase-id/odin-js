57351c1d5d38a691e1bbd40ab03c3de33f4897a2
Stef Coenen <stef@thebrownfox.be>
2024-09-13 11:02:40 +0200

    Polished video playback;

--- packages/common-app/src/media/Video.tsx
+++ packages/common-app/src/media/Video.tsx
@@ -3,11 +3,12 @@ import {
   OdinThumbnailImage,
   OdinVideo,
   OdinVideoProps,
+  useImage,
 } from '@homebase-id/ui-lib';
 import { EmbeddedThumb } from '@homebase-id/js-lib/core';
 import { useMemo, useState } from 'react';
 import { useDotYouClient } from '../hooks';
-import { Loader, Triangle } from '../ui/Icons';
+import { Triangle } from '../ui/Icons';
 
 export interface VideoProps extends Omit<OdinVideoProps, 'dotYouClient'> {
   previewThumbnail?: EmbeddedThumb;
@@ -38,16 +39,18 @@ export interface VideoClickToLoadProps extends Omit<OdinVideoProps, 'dotYouClien
 }
 
 export const VideoClickToLoad = ({
-  preload = true,
+  preload = false,
   previewThumbnail,
   probablyEncrypted,
   ...props
 }: VideoClickToLoadProps) => {
   const dotYouClient = useDotYouClient().getDotYouClient();
+
   const [loadVideo, setLoadVideo] = useState(false);
-  const [shouldFallback, setShouldFallback] = useState(false);
-  const [previewLoaded, setPreviewLoaded] = useState(false);
+  const [imageHasError, setImageHasError] = useState(false);
   const [playingVideo, setPlayingVideo] = useState(false);
+
+  const [previewLoaded, setPreviewLoaded] = useState(false);
 
   return (
     <div
@@ -57,53 +60,76 @@ export const VideoClickToLoad = ({
         setLoadVideo(true);
       }}
     >
-      {shouldFallback ? (
-        loadVideo ? null : (
+      {/* OdinPreviewImage provides the aspect ratio spacing; If it had an error, we let the video decide on the necessary size */}
+      {!imageHasError ? (
+        <OdinPreviewImage
+          dotYouClient={dotYouClient}
+          {...props}
+          className={`${props.className || ''} ${previewLoaded || preload || loadVideo ? 'opacity-0' : 'opacity-100'} blur-sm`}
+          previewThumbnail={previewThumbnail}
+          onError={() => setImageHasError(true)}
+        />
+      ) : null}
+
+      {/* We only load the thumbnail if the video isn't already set to preload */}
+      {!loadVideo && !preload ? (
+        imageHasError ? (
           <div className="bg-slate-200 dark:bg-slate-800 aspect-video w-full h-full"></div>
-        )
-      ) : (
-        <>
-          <OdinPreviewImage
-            dotYouClient={dotYouClient}
-            {...props}
-            className={`${props.className || ''} ${previewLoaded ? 'opacity-0' : 'opacity-100'} blur-sm`}
-            previewThumbnail={previewThumbnail}
-          />
+        ) : (
           <OdinThumbnailImage
             dotYouClient={dotYouClient}
             {...props}
-            className={`${props.className || ''} ${playingVideo ? 'opacity-0' : 'opacity-100'} absolute inset-0 blur-sm ${props.className?.includes('object-') ? '' : 'object-cover'}`}
+            className={`${props.className || ''} absolute inset-0 blur-sm ${props.className?.includes('object-') ? '' : 'object-cover'}`}
             loadSize={{ pixelWidth: 100, pixelHeight: 100 }}
             onLoad={() => setPreviewLoaded(true)}
-            onError={() => setShouldFallback(true)}
+            onError={() => setImageHasError(true)}
             probablyEncrypted={probablyEncrypted}
             width={1920}
             height={1080}
           />
-        </>
-      )}
-      <div className="absolute inset-0 flex items-center justify-center z-0">
-        {loadVideo && !playingVideo ? (
-          <Loader className="h-12 w-12" />
-        ) : (
+        )
+      ) : null}
+
+      {preload || loadVideo ? (
+        <OdinVideoWrapper
+          {...props}
+          autoPlay={loadVideo}
+          onPlay={() => setPlayingVideo(true)}
+          className={imageHasError ? 'w-full h-full' : 'absolute inset-0 w-full h-full'}
+        />
+      ) : null}
+
+      {!playingVideo ? (
+        <div className="absolute inset-0 flex items-center justify-center z-0">
           <div className="bg-background/40 rounded-full p-7 border border-foreground/20">
             <Triangle className="text-foreground h-12 w-12" />
           </div>
-        )}
-      </div>
-
-      {preload || loadVideo ? (
-        <OdinVideo
-          dotYouClient={dotYouClient}
-          {...props}
-          autoPlay={loadVideo}
-          className={`z-0 ${shouldFallback ? 'relative' : 'absolute inset-0'} ${
-            props.className || ''
-          } ${(loadVideo && playingVideo) || shouldFallback ? 'opacity-100' : 'pointer-events-none opacity-0'}`}
-          onPlay={() => setPlayingVideo(true)}
-          probablyEncrypted={probablyEncrypted}
-        />
+        </div>
       ) : null}
     </div>
+  );
+};
+
+interface OdinVideoWrapperProps extends Omit<OdinVideoProps, 'dotYouClient'> {
+  probablyEncrypted?: boolean;
+}
+export const OdinVideoWrapper = ({ ...props }: OdinVideoWrapperProps) => {
+  const dotYouClient = useDotYouClient().getDotYouClient();
+
+  const { data: image } = useImage({
+    dotYouClient,
+    probablyEncrypted: props.probablyEncrypted,
+    imageDrive: props.targetDrive,
+    imageFileId: props.fileId,
+    imageFileKey: props.fileKey,
+    imageGlobalTransitId: props.globalTransitId,
+    lastModified: props.lastModified,
+    odinId: props.odinId,
+    preferObjectUrl: true,
+    size: { pixelWidth: 100, pixelHeight: 100 },
+  }).fetch;
+
+  return (
+    <OdinVideo dotYouClient={dotYouClient} {...props} poster={image ? image.url : props.poster} />
   );
 };
