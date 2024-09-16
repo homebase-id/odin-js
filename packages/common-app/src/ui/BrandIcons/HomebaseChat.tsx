e5793a7c35db6ef611b2a8f33e753b7c1423b7d8
Stef Coenen <stef@thebrownfox.be>
2024-09-16 18:13:37 +0200

    Updated logo's;

--- packages/common-app/src/ui/BrandIcons/HomebaseChat.tsx
+++ packages/common-app/src/ui/BrandIcons/HomebaseChat.tsx
@@ -11,38 +11,22 @@ export const HomebaseChat: FC<IconProps> = ({ className }) => {
       xmlns="http://www.w3.org/2000/svg"
       className={`${className}`}
     >
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M459.5 879C691.183 879 879 691.183 879 459.5C879 227.817 691.183 40 459.5 40C227.817 40 40 227.817 40 459.5C40 691.183 227.817 879 459.5 879ZM459.5 919C713.275 919 919 713.275 919 459.5C919 205.725 713.275 0 459.5 0C205.725 0 0 205.725 0 459.5C0 713.275 205.725 919 459.5 919Z"
-        fill="url(#paint0_linear_410_40)"
-      />
+      <rect width="919" height="919" rx="80" fill="url(#paint0_linear_410_40)" />
       <path
         d="M718 443.857C718 559.783 602.511 653.714 460.015 653.714C422.627 653.714 387.154 647.257 355.108 635.654C343.115 644.432 323.565 656.438 300.387 666.528C276.201 677.021 247.076 686 218.154 686C211.604 686 205.759 682.065 203.239 676.012C200.72 669.958 202.131 663.097 206.666 658.456L206.968 658.154C207.27 657.851 207.673 657.447 208.278 656.741C209.386 655.53 211.1 653.613 213.216 650.99C217.348 645.945 222.89 638.479 228.534 629.197C238.611 612.449 248.185 590.454 250.1 565.736C219.867 531.432 202.03 489.36 202.03 443.857C202.03 327.931 317.519 234 460.015 234C602.511 234 718 327.931 718 443.857Z"
-        fill="url(#paint1_linear_410_40)"
+        fill="white"
       />
       <defs>
         <linearGradient
           id="paint0_linear_410_40"
-          x1="613.242"
-          y1="274.057"
-          x2="309.406"
-          y2="561.747"
+          x1="795.951"
+          y1="986.767"
+          x2="375.677"
+          y2="-58.4083"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stopColor="#C68CFF" />
-          <stop offset="1" stopColor="#8CD9FF" />
-        </linearGradient>
-        <linearGradient
-          id="paint1_linear_410_40"
-          x1="613.242"
-          y1="274.057"
-          x2="309.406"
-          y2="561.747"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stopColor="#C68CFF" />
-          <stop offset="1" stopColor="#8CD9FF" />
+          <stop offset="0.265" stopColor="#191272" />
+          <stop offset="1" stopColor="#ED0342" />
         </linearGradient>
       </defs>
     </svg>
