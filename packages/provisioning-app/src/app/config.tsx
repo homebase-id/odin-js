66e423d13fe61b425e52819e0f0bb98934783b7b
Stef Coenen <stef@thebrownfox.be>
2024-09-25 09:43:35 +0200

    Added homebase themed provisioning;

--- packages/provisioning-app/src/app/config.tsx
+++ packages/provisioning-app/src/app/config.tsx
@@ -1,6 +1,8 @@
 import ravenLogo from '../assets/raven-logo.svg';
+import homebaseLogo from '../assets/homebase-logo.svg';
 
-export const config = {
+const ravenHostingConfig = {
+  id: 'ravenhosting',
   brandName: 'Raven Hosting',
   brandSlogan: 'Keeping your life private',
   logo: ravenLogo,
@@ -11,3 +13,21 @@ export const config = {
   termsAndConditionsLink: 'https://ravenhosting.cloud/terms-and-conditions',
   privacyPolicyLink: 'https://ravenhosting.cloud/privacy-policy',
 };
+
+const homebaseHostingConfig = {
+  id: 'homebase',
+  brandName: 'Homebase.id',
+  brandSlogan: 'Reclaim the internet',
+  logo: homebaseLogo,
+  primaryClassName:
+    'border border-indigo-500 bg-indigo-500 text-white transition-colors hover:bg-transparent hover:text-indigo-800',
+  secondaryClassName:
+    'border border-indigo-500 bg-white text-indigo-800 transition-colors hover:bg-indigo-500 hover:text-white',
+  termsAndConditionsLink: 'https://homebase.id/terms-and-conditions',
+  privacyPolicyLink: 'https://homebase.id/privacy-policy',
+};
+
+console.log(import.meta.env.brand);
+
+export const config =
+  import.meta.env.VITE_BRAND === ravenHostingConfig.id ? ravenHostingConfig : homebaseHostingConfig;
