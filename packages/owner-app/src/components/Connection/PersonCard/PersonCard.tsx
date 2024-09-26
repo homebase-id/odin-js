6d8bcd0c5f39cdfdb6705f6081dbf87bd65dd8c8
Stef Coenen <stef@thebrownfox.be>
2024-09-26 17:09:14 +0200

    Updated member selection for better visibility;

--- packages/owner-app/src/components/Connection/PersonCard/PersonCard.tsx
+++ packages/owner-app/src/components/Connection/PersonCard/PersonCard.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 
 import ContactImage from '../ContactImage/ContactImage';
-import { HybridLink } from '@homebase-id/common-app';
+import { Checkbox, HybridLink } from '@homebase-id/common-app';
 
 export interface PersonCardProps {
   odinId: string;
@@ -27,16 +27,18 @@ const PersonCard = ({
       <div
         className={`h-full rounded-md border bg-white transition-colors dark:bg-gray-800 ${
           isChecked
-            ? 'border-4 border-indigo-500 dark:border-indigo-700'
+            ? 'border-4 border-indigo-500 dark:border-indigo-500'
             : isChecked === false
-              ? 'border-4'
-              : 'border-gray-200 border-opacity-60 dark:border-gray-800'
-        }
-        ${href ? 'cursor-pointer hover:shadow-md hover:dark:shadow-slate-600' : ''}`}
+              ? 'border-4 dark:border-gray-800'
+              : 'border-gray-200 border-opacity-60 dark:border-gray-900'
+        } ${href ? 'cursor-pointer hover:shadow-md hover:dark:shadow-slate-600' : ''}`}
         onClick={onClick}
       >
         <ContactImage odinId={odinId} canSave={canSave} />
         <div className="p-2">{children}</div>
+        {isChecked === undefined ? null : (
+          <Checkbox checked={isChecked} className="absolute right-2 top-2" />
+        )}
       </div>
     </HybridLink>
   );
