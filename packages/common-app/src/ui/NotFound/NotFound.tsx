dae32251e6aa6dae5496b5257dc5c0cdbe0b6a97
Stef Coenen <stef@thebrownfox.be>
2024-09-26 14:34:17 +0200

    Updated 404;

--- packages/common-app/src/ui/NotFound/NotFound.tsx
+++ packages/common-app/src/ui/NotFound/NotFound.tsx
@@ -1,7 +1,17 @@
+import { ActionButton } from '../Buttons/ActionButton';
+
 export const NotFound = () => {
   return (
-    <section className="flex h-full grow flex-col bg-red-400 py-5">
-      <h1 className="m-auto text-center text-5xl text-white">404&apos;d! Nothing to see here.</h1>
+    <section className="flex h-full grow flex-col bg-red-50 py-5">
+      <h1 className="my-auto text-center text-3xl font-medium">
+        404&apos;d! Nothing to see here.
+        <small className="block font-light">Take your control</small>
+        <div className="flex flex-row justify-center font-normal text-base">
+          <ActionButton type="secondary" onClick={() => window.history.back()}>
+            Back
+          </ActionButton>
+        </div>
+      </h1>
     </section>
   );
 };
