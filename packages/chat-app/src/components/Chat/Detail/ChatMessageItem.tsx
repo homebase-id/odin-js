8b63cfdc70716774450b1d93a86ebf0f5ecbf2e1
Stef Coenen <stef@thebrownfox.be>
2024-10-03 14:41:09 +0200

    Fix of chat mention highlighting;

--- packages/chat-app/src/components/Chat/Detail/ChatMessageItem.tsx
+++ packages/chat-app/src/components/Chat/Detail/ChatMessageItem.tsx
@@ -173,11 +173,12 @@ const ChatTextMessageBody = ({
   );
 };
 
-const urlAndMentionRegex = new RegExp(/(https?:\/\/[^\s]+|@[^\s]+)/);
+const urlAndMentionRegex = new RegExp(/(https?:\/\/[^\s]|(?:^|\s|[\r\n])@[^\s]+)/);
 const urlRegex = new RegExp(
   /https?:\/\/([a-zA-Z0-9.-]+\.[a-zA-Z]{2,}|(localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(:\d{1,5})?)(\/[^\s]*)?/
 );
-const mentionRegex = new RegExp(/@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/);
+const mentionRegex = new RegExp(/(?:^|\s|[\r\n])@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/);
+
 const ParagraphWithLinks = ({ text, className }: { text: string; className?: string }) => {
   const splitUpText = text.split(urlAndMentionRegex);
 
@@ -197,16 +198,20 @@ const ParagraphWithLinks = ({ text, className }: { text: string; className?: str
             </a>
           );
         } else if (mentionRegex.test(part)) {
+          const trimmedPart = part.trim();
           return (
-            <a
-              key={index}
-              href={`https://${part.slice(1)}`}
-              target="_blank"
-              rel="noreferrer"
-              className="break-all text-primary underline"
-            >
-              {part}
-            </a>
+            <>
+              {part.slice(0, 1) === ' ' ? ' ' : null}
+              <a
+                key={index}
+                href={`https://${part.slice(1)}`}
+                target="_blank"
+                rel="noreferrer"
+                className="break-all text-primary hover:underline"
+              >
+                {trimmedPart}
+              </a>
+            </>
           );
         }
         return part;
