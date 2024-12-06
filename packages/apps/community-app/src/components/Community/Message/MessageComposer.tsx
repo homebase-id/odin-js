4d7dab15514291eb6a9832bcab8fcbde9ac27574
Stef Coenen <stef@thebrownfox.be>
2024-12-06 15:29:05 +0100

    Removed file selector mimeType filter;

--- packages/apps/community-app/src/components/Community/Message/MessageComposer.tsx
+++ packages/apps/community-app/src/components/Community/Message/MessageComposer.tsx
@@ -196,7 +196,7 @@ export const MessageComposer = ({
               <FileSelector
                 onChange={(files) => setFiles(files.map((file) => ({ file })))}
                 className="my-auto px-2 py-1 text-foreground text-opacity-30 hover:text-opacity-100"
-                accept="image/png, image/jpeg, image/tiff, image/webp, image/svg+xml, image/gif, video/mp4, audio/mp3, application/pdf"
+                accept="*"
                 maxSize={HUNDRED_MEGA_BYTES}
               >
                 <Plus className="h-5 w-5" />
