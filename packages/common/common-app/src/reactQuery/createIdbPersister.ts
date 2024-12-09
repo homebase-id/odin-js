8c81c73a6320974835438774b46759cecb91bff9
Stef Coenen <stef@thebrownfox.be>
2024-12-09 15:47:36 +0100

    Add info and docs on what got us there;

--- packages/common/common-app/src/reactQuery/createIdbPersister.ts
+++ packages/common/common-app/src/reactQuery/createIdbPersister.ts
@@ -6,6 +6,12 @@ import { debounce } from 'lodash-es';
  * Creates an Indexed DB persister
  * @see https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API
  */
+// Persiter built like recommended by React Query: https://tanstack.com/query/v4/docs/framework/react/plugins/persistQueryClient#building-a-persister
+// But got issues around persiting a lot, especially on Firefox https://github.com/TanStack/query/issues/5854; So we use this throttle fix: https://github.com/TanStack/query/issues/5854#issuecomment-1675968101
+
+// Other options:
+// Can't use the experimental CreatePersister https://github.com/TanStack/query/issues/6310
+// Optional extension at some point: https://github.com/epicweb-dev/cachified
 export function createIDBPersister(idbValidKey: IDBValidKey) {
   return {
     persistClient: debounce(async (client: PersistedClient) => {
