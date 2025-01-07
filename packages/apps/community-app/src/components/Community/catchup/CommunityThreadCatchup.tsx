4d63cce04e3640afef4d0a1b02e751a59d70e5e5
Stef Coenen <stef@thebrownfox.be>
2025-01-07 09:26:57 +0100

    WIP threads page;

--- packages/apps/community-app/src/components/Community/catchup/CommunityThreadCatchup.tsx
+++ packages/apps/community-app/src/components/Community/catchup/CommunityThreadCatchup.tsx
@@ -1,11 +1,18 @@
 import { HomebaseFile } from '@homebase-id/js-lib/core';
 import { CommunityDefinition } from '../../../providers/CommunityDefinitionProvider';
 import { CommunityHistory } from '../channel/CommunityHistory';
-import { AuthorName, COMMUNITY_ROOT_PATH, FakeAnchor } from '@homebase-id/common-app';
+import {
+  ActionLink,
+  AuthorName,
+  COMMUNITY_ROOT_PATH,
+  ErrorBoundary,
+  t,
+} from '@homebase-id/common-app';
 import { useCommunityMessage } from '../../../hooks/community/messages/useCommunityMessage';
 import { useCommunityChannel } from '../../../hooks/community/channels/useCommunityChannel';
 import { ThreadMeta } from '../../../hooks/community/threads/useCommunityThreads';
-import React from 'react';
+import React, { useState } from 'react';
+import { MessageComposer } from '../Message/composer/MessageComposer';
 
 export const CommunityThreadCatchup = ({
   community,
@@ -15,6 +22,7 @@ export const CommunityThreadCatchup = ({
   threadMeta: ThreadMeta;
 }) => {
   const communityId = community.fileMetadata.appData.uniqueId;
+  const [participants, setParticipants] = useState<string[] | null>();
 
   const { data: channel } = useCommunityChannel({
     odinId: community.fileMetadata.senderOdinId,
@@ -34,38 +42,55 @@ export const CommunityThreadCatchup = ({
 
   return (
     <div className="rounded-md border bg-background hover:shadow-md">
-      <FakeAnchor
-        href={`${COMMUNITY_ROOT_PATH}/${community.fileMetadata.senderOdinId}/${communityId}/${channel.fileMetadata.appData.uniqueId}/${threadMeta.threadId}/thread`}
-      >
-        <div className="flex flex-col bg-slate-200 px-2 py-2 dark:bg-slate-800">
-          <p className="text-lg"># {channel.fileMetadata.appData.content.title}</p>
-          <p className="text-sm text-slate-400">
-            {threadMeta.participants.map((participant, index) => (
-              <React.Fragment key={participant}>
-                <AuthorName odinId={participant} excludeLink={true} />
-                {threadMeta.participants.length > 1 ? (
-                  <>
-                    {index < threadMeta.participants.length - 2
-                      ? ', '
-                      : index < threadMeta.participants.length - 1
-                        ? ' and '
-                        : ''}
-                  </>
-                ) : null}
-              </React.Fragment>
-            ))}
-          </p>
-        </div>
+      <div className="flex flex-col bg-slate-200 px-2 py-2 dark:bg-slate-800">
+        <p className="text-lg"># {channel.fileMetadata.appData.content.title}</p>
+        <p className="text-sm text-slate-400">
+          {threadMeta.participants.map((participant, index) => (
+            <React.Fragment key={participant}>
+              <AuthorName odinId={participant} excludeLink={true} />
+              {threadMeta.participants.length > 1 ? (
+                <>
+                  {index < threadMeta.participants.length - 2
+                    ? ', '
+                    : index < threadMeta.participants.length - 1
+                      ? ' and '
+                      : ''}
+                </>
+              ) : null}
+            </React.Fragment>
+          ))}
+        </p>
+      </div>
 
-        <div className="pointer-events-none relative">
-          <CommunityHistory
+      {/* <div className="flex flex-row justify-center py-2">
+        <ActionLink
+          href={`${COMMUNITY_ROOT_PATH}/${community.fileMetadata.senderOdinId}/${communityId}/${channel.fileMetadata.appData.uniqueId}/${threadMeta.threadId}/thread`}
+          type="secondary"
+          size="none"
+          className="px-2 py-1 text-sm"
+        >
+          {t('See older messages')}
+        </ActionLink>
+      </div> */}
+      <CommunityHistory
+        community={community}
+        channel={channel}
+        origin={originMessage}
+        setParticipants={setParticipants}
+        alignTop={true}
+      />
+      <ErrorBoundary>
+        {originMessage ? (
+          <MessageComposer
             community={community}
+            thread={originMessage}
             channel={channel}
-            origin={originMessage}
-            alignTop={true}
+            key={threadMeta.threadId}
+            threadParticipants={participants || undefined}
+            className="mt-auto xl:mt-0"
           />
-        </div>
-      </FakeAnchor>
+        ) : null}
+      </ErrorBoundary>
     </div>
   );
 };
