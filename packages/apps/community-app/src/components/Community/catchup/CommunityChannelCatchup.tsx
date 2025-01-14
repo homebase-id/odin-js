9a089bf3e12192f0e22a542e22aef70af2637684
Stef Coenen <stef@thebrownfox.be>
2025-01-14 10:23:24 +0100

    Made channel catchup smarter on maxAge;

--- packages/apps/community-app/src/components/Community/catchup/CommunityChannelCatchup.tsx
+++ packages/apps/community-app/src/components/Community/catchup/CommunityChannelCatchup.tsx
@@ -2,10 +2,9 @@ import { HomebaseFile } from '@homebase-id/js-lib/core';
 import { CommunityDefinition } from '../../../providers/CommunityDefinitionProvider';
 import { ChannelWithRecentMessage } from '../../../hooks/community/channels/useCommunityChannelsWithRecentMessages';
 import { CommunityHistory } from '../channel/CommunityHistory';
-import { ActionLink, COMMUNITY_ROOT_PATH } from '@homebase-id/common-app';
+import { ActionLink, COMMUNITY_ROOT_PATH, useDotYouClientContext } from '@homebase-id/common-app';
 import { ExternalLink } from '@homebase-id/common-app/icons';
-
-const OneDayInMs = 24 * 60 * 60 * 1000;
+import { useCommunityMetadata } from '../../../hooks/community/useCommunityMetadata';
 
 export const CommunityChannelCatchup = ({
   community,
@@ -16,8 +15,28 @@ export const CommunityChannelCatchup = ({
 }) => {
   const communityId = community.fileMetadata.appData.uniqueId;
   const channelLink = `${COMMUNITY_ROOT_PATH}/${community.fileMetadata.senderOdinId}/${communityId}/${channel.fileMetadata.appData.uniqueId}`;
+  const { data: metadata } = useCommunityMetadata({
+    odinId: community.fileMetadata.senderOdinId,
+    communityId,
+  }).single;
+  const channelLastRead =
+    metadata?.fileMetadata.appData.content.channelLastReadTime[
+      channel.fileMetadata.appData.uniqueId as string
+    ];
 
+  const loggedInIdentity = useDotYouClientContext().getLoggedInIdentity();
   if (!channel.lastMessage) return null;
+
+  const todayDate = new Date(channel.lastMessage?.fileMetadata.created);
+  todayDate.setHours(0, 0, 0, 0);
+  const defaultMaxAge = todayDate.getTime();
+
+  const maxAge =
+    (channel.lastMessage?.fileMetadata.originalAuthor !== loggedInIdentity &&
+      channelLastRead &&
+      channelLastRead < defaultMaxAge &&
+      channelLastRead) ||
+    defaultMaxAge;
 
   return (
     <div className="pb-3 last-of-type:pb-0">
@@ -41,7 +60,7 @@ export const CommunityChannelCatchup = ({
               count: 5,
               targetLink: channelLink,
             }}
-            maxAge={channel.lastMessage?.fileMetadata.created - OneDayInMs}
+            maxAge={maxAge}
           />
         </div>
       </div>
