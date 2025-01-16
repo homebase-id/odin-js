ab13d1ca1454406c8a2a52590c55edcf286f2776
Stef Coenen <stef@thebrownfox.be>
2025-01-16 09:44:23 +0100

    Improved notification registration info to provider more info and block a user from trying to enable if it will fail; (#526)

--- packages/apps/owner-app/src/index.tsx
+++ packages/apps/owner-app/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './app/App';
+import { OWNER_ROOT } from '@homebase-id/common-app';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
@@ -11,7 +12,12 @@ root.render(
 );
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register(
-    import.meta.env.MODE === 'production' ? '/owner/sw.js' : '/owner/dev-sw.js?dev-sw'
-  );
+  if (window.location.pathname === OWNER_ROOT) {
+    window.location.assign(window.location.href + '/');
+  } else {
+    navigator.serviceWorker.register(
+      import.meta.env.MODE === 'production' ? '/owner/sw.js' : '/owner/dev-sw.js?dev-sw',
+      { scope: `.${OWNER_ROOT}/` }
+    );
+  }
 }
