8fc8ece68e982f5acec519609548d58c4b60ec69
todd mitchell <1205927+toddmitchell@users.noreply.github.com>
2025-06-24 11:10:18 -0700

    adjusting to use remote payload source (#704)
    * adjusting to use remote payload source
    
    * renaming
    
    * testing and back/forth insanity; more testing required
    
    * renamed again
    
    * removed edit post for all but my articles on my feed
    
    * tiny marker when files are using a datasource
    
    * alias --
    
    ---------
    
    Co-authored-by: toddmitchell <t@lighttheory.co>

--- packages/apps/owner-app/src/templates/Dashboard/FeedTeaser.tsx
+++ packages/apps/owner-app/src/templates/Dashboard/FeedTeaser.tsx
@@ -1,135 +1,144 @@
 import {
-  t,
-  FakeAnchor,
-  useSocialFeed,
-  AuthorImage,
-  AuthorName,
-  DoubleClickHeartForMedia,
-  ErrorBoundary,
-  PostBody,
-  PostMeta,
-  useChannel,
-  useCheckIdentity,
-  useDotYouClientContext,
-  LoadingBlock,
+    t,
+    FakeAnchor,
+    useSocialFeed,
+    AuthorImage,
+    AuthorName,
+    DoubleClickHeartForMedia,
+    ErrorBoundary,
+    PostBody,
+    PostMeta,
+    useChannel,
+    useCheckIdentity,
+    useDotYouClientContext,
+    LoadingBlock,
 } from '@homebase-id/common-app';
-import { UnreachableIdentity } from '@homebase-id/feed-app/src/components/SocialFeed/UnreachableIdentity';
-import { HomebaseFile } from '@homebase-id/js-lib/core';
-import { PostContent } from '@homebase-id/js-lib/public';
+import {UnreachableIdentity} from '@homebase-id/feed-app/src/components/SocialFeed/UnreachableIdentity';
+import {HomebaseFile} from '@homebase-id/js-lib/core';
+import {PostContent} from '@homebase-id/js-lib/public';
 
 const POSTS_TO_SHOW = 2;
-export const FeedTeaser = ({ className }: { className?: string }) => {
-  const { data: posts, isFetched } = useSocialFeed({ pageSize: POSTS_TO_SHOW }).fetchAll;
-  const latestPosts = posts?.pages?.[0]?.results;
+export const FeedTeaser = ({className}: { className?: string }) => {
+    const {data: posts, isFetched} = useSocialFeed({pageSize: POSTS_TO_SHOW}).fetchAll;
+    const latestPosts = posts?.pages?.[0]?.results;
 
-  const hasPosts = latestPosts && latestPosts?.length;
+    const hasPosts = latestPosts && latestPosts?.length;
 
-  return (
-    <div className={className}>
-      <div className="mb-4 flex flex-row items-center justify-between">
-        <p className="text-2xl">{t('What has everyone been up to?')}</p>
-      </div>
-      <FakeAnchor className="w-full" href={hasPosts ? `/apps/feed` : `/owner/connections`}>
-        <div className="pointer-events-none flex w-full flex-col gap-4">
-          {hasPosts ? (
-            latestPosts.slice(0, POSTS_TO_SHOW).map((post, index) => (
-              <div
-                className={`w-full rounded-md bg-background ${index !== 0 ? 'hidden lg:block' : ''}`}
-                key={post.fileId}
-              >
-                <PostTeaser postFile={post} odinId={post.fileMetadata.senderOdinId} />
-              </div>
-            ))
-          ) : isFetched ? (
-            <p className="rounded-md bg-background px-4 py-4 text-slate-400">
-              {t('Fill up your feed, by following people, or connecting with other identities')}
-            </p>
-          ) : (
-            <>
-              <LoadingBlock className="h-44 w-full bg-background" />
-              <LoadingBlock className="h-44 w-full bg-background" />
-            </>
-          )}
+    return (
+        <div className={className}>
+            <div className="mb-4 flex flex-row items-center justify-between">
+                <p className="text-2xl">{t('What has everyone been up to?')}</p>
+            </div>
+            <FakeAnchor className="w-full" href={hasPosts ? `/apps/feed` : `/owner/connections`}>
+                <div className="pointer-events-none flex w-full flex-col gap-4">
+                    {hasPosts ? (
+                        latestPosts.slice(0, POSTS_TO_SHOW).map((post, index) => (
+                            <div
+                                className={`w-full rounded-md bg-background ${index !== 0 ? 'hidden lg:block' : ''}`}
+                                key={post.fileId}
+                            >
+                                <PostTeaser postFile={post}/>
+                            </div>
+                        ))
+                    ) : isFetched ? (
+                        <p className="rounded-md bg-background px-4 py-4 text-slate-400">
+                            {t('Fill up your feed, by following people, or connecting with other identities')}
+                        </p>
+                    ) : (
+                        <>
+                            <LoadingBlock className="h-44 w-full bg-background"/>
+                            <LoadingBlock className="h-44 w-full bg-background"/>
+                        </>
+                    )}
+                </div>
+            </FakeAnchor>
         </div>
-      </FakeAnchor>
-    </div>
-  );
+    );
 };
 
 const PostTeaser = ({
-  postFile,
-  odinId,
-  className,
-}: {
-  postFile: HomebaseFile<PostContent>;
-  odinId?: string;
-  className?: string;
+                        postFile,
+                        className,
+                    }: {
+    postFile: HomebaseFile<PostContent>;
+    className?: string;
 }) => {
-  const post = postFile.fileMetadata.appData.content;
-  const loggedOnIdentity = useDotYouClientContext().getLoggedInIdentity();
-  const isExternal = odinId && odinId !== loggedOnIdentity;
+    const post = postFile.fileMetadata.appData.content;
+    const loggedOnIdentity = useDotYouClientContext().getLoggedInIdentity();
 
-  const { data: identityAccessible } = useCheckIdentity(isExternal ? odinId : undefined);
+    let odinId = postFile.fileMetadata.senderOdinId
+    let channelId = post.channelId;
 
-  const { data: channel } = useChannel({
-    odinId: isExternal ? odinId : undefined,
-    channelKey: post.channelId,
-  }).fetch;
+    let usingRemotePayload = false;
+    if (postFile.fileMetadata.dataSource?.payloadsAreRemote === true) {
+        odinId = postFile.fileMetadata.dataSource.identity;
+        channelId = postFile.fileMetadata.dataSource.driveId;
+        usingRemotePayload = true;
+    }
 
-  const authorOdinId = postFile.fileMetadata.originalAuthor || odinId;
+    const isExternal = odinId && odinId !== loggedOnIdentity;
 
-  if (identityAccessible === false && isExternal)
-    return <UnreachableIdentity postFile={postFile} className={className} odinId={odinId} />;
+    const {data: identityAccessible} = useCheckIdentity(isExternal ? odinId : undefined);
 
-  return (
-    <div className={`w-full break-words rounded-lg ${className ?? ''}`}>
-      <ErrorBoundary>
-        <div
-          className={`relative flex h-full flex-col rounded-lg border-gray-200 border-opacity-60 transition-colors dark:border-gray-800 lg:border`}
-        >
-          <div className="flex flex-row gap-4 px-3 py-3 sm:px-4">
-            <div className="flex-shrink-0 py-1">
-              <div className="h-10 w-10 sm:h-12 sm:w-12 md:h-[4rem] md:w-[4rem]">
-                <AuthorImage
-                  odinId={authorOdinId}
-                  className="h-10 w-10 rounded-full sm:h-12 sm:w-12 md:h-[4rem] md:w-[4rem]"
-                />
-              </div>
-            </div>
-            <div className="flex w-20 flex-grow flex-col">
-              <div className="mb-1 flex flex-col text-foreground text-opacity-60 md:flex-row md:flex-wrap md:items-center">
-                <h2>
-                  <AuthorName odinId={authorOdinId} />
-                </h2>
-                <span className="hidden px-2 leading-4 md:block">·</span>
+    const {data: channel} = useChannel({
+        odinId: isExternal ? odinId : undefined,
+        channelKey: channelId
+    }).fetch;
 
-                <PostMeta
-                  postFile={postFile}
-                  channel={channel || undefined}
-                  odinId={odinId}
-                  authorOdinId={authorOdinId}
-                  excludeContextMenu={true}
-                />
-              </div>
+    const authorOdinId = postFile.fileMetadata.originalAuthor || odinId;
 
-              <PostBody
-                post={post}
-                odinId={odinId}
-                fileId={postFile.fileId}
-                globalTransitId={postFile.fileMetadata.globalTransitId}
-                lastModified={postFile.fileMetadata.updated}
-                payloads={postFile.fileMetadata.payloads}
-              />
-            </div>
-          </div>
-          <DoubleClickHeartForMedia
-            odinId={odinId}
-            postFile={postFile}
-            onClick={undefined}
-            className="mb-4"
-          />
+    if (identityAccessible === false && isExternal)
+        return <UnreachableIdentity postFile={postFile} className={className} odinId={odinId}/>;
+
+    return (
+        <div className={`w-full break-words rounded-lg ${className ?? ''}`}>
+            <ErrorBoundary>
+                <div
+                    className={`relative flex h-full flex-col rounded-lg border-gray-200 border-opacity-60 transition-colors dark:border-gray-800 lg:border`}
+                >
+                    <div className="flex flex-row gap-4 px-3 py-3 sm:px-4">
+                        <div className="flex-shrink-0 py-1">
+                            <div className="h-10 w-10 sm:h-12 sm:w-12 md:h-[4rem] md:w-[4rem]">
+                                <AuthorImage
+                                    odinId={authorOdinId}
+                                    className="h-10 w-10 rounded-full sm:h-12 sm:w-12 md:h-[4rem] md:w-[4rem]"
+                                />
+                            </div>
+                        </div>
+                        <div className="flex w-20 flex-grow flex-col">
+                            <div className="mb-1 flex flex-col text-foreground text-opacity-60 md:flex-row md:flex-wrap md:items-center">
+                                <h2>
+                                    <AuthorName odinId={authorOdinId}/>
+                                </h2>
+                                <span className="hidden px-2 leading-4 md:block">{usingRemotePayload ? "~" : "·"}</span>
+
+                                <PostMeta
+                                    postFile={postFile}
+                                    channel={channel || undefined}
+                                    odinId={odinId}
+                                    authorOdinId={authorOdinId}
+                                    excludeContextMenu={true}
+                                />
+                            </div>
+
+                            <PostBody
+                                post={post}
+                                odinId={odinId}
+                                fileId={postFile.fileId}
+                                globalTransitId={postFile.fileMetadata.globalTransitId}
+                                lastModified={postFile.fileMetadata.updated}
+                                payloads={postFile.fileMetadata.payloads}
+                            />
+                        </div>
+                    </div>
+                    <DoubleClickHeartForMedia
+                        odinId={odinId}
+                        postFile={postFile}
+                        onClick={undefined}
+                        className="mb-4"
+                    />
+                </div>
+            </ErrorBoundary>
         </div>
-      </ErrorBoundary>
-    </div>
-  );
+    );
 };
