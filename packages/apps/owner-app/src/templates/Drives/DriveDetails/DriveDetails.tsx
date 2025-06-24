02d35fe20f874d1df46f0a0baf996877570c1d4f
toddmitchell <t@lighttheory.co>
2025-06-24 09:53:40 -0700

    alias --

--- packages/apps/owner-app/src/templates/Drives/DriveDetails/DriveDetails.tsx
+++ packages/apps/owner-app/src/templates/Drives/DriveDetails/DriveDetails.tsx
@@ -128,7 +128,7 @@ const DriveDetails = () => {
             </>
           ) : null}
           <li className="my-3 border-b border-slate-200 dark:border-slate-800"></li>
-          <li>Alias: {driveDef.targetDriveInfo.alias}</li>
+          <li>Id: {driveDef.targetDriveInfo.alias}</li>
           <li>Type: {driveDef.targetDriveInfo.type}</li>
         </ul>
       </Section>
