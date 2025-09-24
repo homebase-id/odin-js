68a2e0878b65e0495a3cf30443c7bb48068dbe2b
toddmitchell <t@lighttheory.co>
2025-09-24 13:03:55 -0500

    state issues

--- packages/apps/owner-app/src/templates/Security/Dialog/ChangeRecoveryEmailDialog.tsx
+++ packages/apps/owner-app/src/templates/Security/Dialog/ChangeRecoveryEmailDialog.tsx
@@ -3,150 +3,165 @@ import {createPortal} from 'react-dom';
 import {ActionButton, DialogWrapper, Input, Label, t, usePortal,} from '@homebase-id/common-app';
 import {Check} from "@homebase-id/common-app/icons";
 import {
-    getRecoveryInfo,
-    RecoveryInfo,
-    updateRecoveryEmail
+  getRecoveryInfo,
+  RecoveryInfo,
+  updateRecoveryEmail
 } from "../../../provider/auth/SecurityHealthProvider";
 import {MaskedInput} from "../../../components/Password/MaskedInput";
 
 
 export const ChangeRecoveryEmailDialog = ({
-                                              title,
-                                              isOpen,
-                                              verifyOnly,
-                                              defaultEmail,
-                                              onConfirm,
-                                              onCancel,
+                                            title,
+                                            info,
+                                            isOpen,
+                                            verifyOnly,
+                                            onConfirm,
+                                            onCancel,
                                           }: {
-    title: string;
-    isOpen: boolean;
-    verifyOnly: boolean;
-    defaultEmail: string;
-    onConfirm: () => void;
-    onCancel: () => void;
+  title: string;
+  info: RecoveryInfo,
+  isOpen: boolean;
+  verifyOnly: boolean;
+  onConfirm: () => void;
+  onCancel: () => void;
 }) => {
 
-    const target = usePortal('modal-container');
-    const [state, setState] = useState<'loading' | 'error' | 'success' | 'idle'>('idle');
-    const [validationError, setValidationError] = useState<string | null>(null);
-    const [currentPassword, setCurrentPassword] = useState<string>('');
-    const [newRecoveryEmail, setNewRecoveryEmail] = useState<string>(defaultEmail);
-    const [info, setInfo] = useState<RecoveryInfo | null>()
-    const [view, setView] = useState<'prompt' | 'done'>('prompt');
+  const target = usePortal('modal-container');
+  const [state, setState] = useState<'loading' | 'error' | 'success' | 'idle'>('idle');
+  const [validationError, setValidationError] = useState<string | null>(null);
+  const [currentPassword, setCurrentPassword] = useState<string>('');
+  const [newRecoveryEmail, setNewRecoveryEmail] = useState<string>('');
+  // const [info, setInfo] = useState<RecoveryInfo | null>()
+  const [view, setView] = useState<'prompt' | 'done'>('prompt');
 
-    const reset = async () => {
-        setCurrentPassword('');
-        setState('idle');
-        setValidationError(null);
-        setView('prompt');
+  const reset = async () => {
+    setCurrentPassword('');
+    setState('idle');
+    setValidationError(null);
+    setView('prompt');
 
-        const info = await getRecoveryInfo();
-        setInfo(info);
+    // const info = await getRecoveryInfo();
+    // setInfo(info);
+  }
+
+  const doUpdateRecoveryEmail = async () => {
+    setState('loading');
+    try {
+      await updateRecoveryEmail(newRecoveryEmail, currentPassword);
+      setView('done');
+      setValidationError(null);
+    } catch (err: any) {
+      setValidationError(err.message);
+    } finally {
+      setState('idle');
     }
+  }
 
-    const doUpdateRecoveryEmail = async () => {
-        setState('loading');
-        try {
-            await updateRecoveryEmail(newRecoveryEmail, currentPassword);
-            setView('done');
-            setValidationError(null);
-        } catch (err: any) {
-            setValidationError(err.message);
-        } finally {
-            setState('idle');
+  if (!isOpen) return null;
+
+  if (info == null) {
+    return (<DialogWrapper
+      title={title}
+      onClose={() => {
+        reset();
+        onCancel();
+      }}
+      keepOpenOnBlur={true}
+      size="2xlarge">
+      <>
+        <div className="flex flex-col items-center justify-center h-full">Error</div>
+      </>
+    </DialogWrapper>)
+  }
+
+  const dialog = (
+    <DialogWrapper
+      title={title}
+      onClose={() => {
+        reset();
+        onCancel();
+      }}
+      keepOpenOnBlur={true}
+      size="2xlarge">
+      <>
+        {view == 'done' &&
+            <div className="mb-2 rounded-lg px-3 py-2">
+                <div className="flex items-center ">
+                    <Check className="h-5 w-5 mr-2 text-green-600"/>
+                    <span
+                        className="font-medium">{t('We have sent an email to your recovery email.  Please follow the instructions in this email to finalize your update.')}</span>
+                </div>
+
+                <div className="mt-3">
+                    <ActionButton
+                        onClick={() => {
+                          reset();
+                          onConfirm();
+                        }}>
+                      {t('Close')}
+                    </ActionButton>
+                </div>
+            </div>
         }
-    }
 
-    if (!isOpen) return null;
+        {view == 'prompt' &&
+            <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
 
-    const dialog = (
-        <DialogWrapper
-            title={title}
-            onClose={() => {
-                reset();
-                onCancel();
-            }}
-            keepOpenOnBlur={true}
-            size="2xlarge">
-            <>
-                {view == 'done' &&
-                    <div className="mb-2 rounded-lg px-3 py-2">
-                        <div className="flex items-center ">
-                            <Check className="h-5 w-5 mr-2 text-green-600"/>
-                            <span
-                                className="font-medium">{t('We have sent an email to your recovery email.  Please follow the instructions in this email to finalize your update.')}</span>
-                        </div>
+                  if (e.currentTarget.reportValidity()) {
+                    doUpdateRecoveryEmail();
+                  }
+                }}
+            >
+                <div className="mb-2">
+                    <Label>{t('Your current recovery email')}</Label>
+                    <span>{info?.email}</span>
+                </div>
+                <hr className="my-4"/>
+                <div className="mb-2">
+                  {verifyOnly ?
+                    <Label>{t('Recovery email')}</Label> :
+                    <Label>{t('New Recovery email')}</Label>}
+                    <Input
+                        required
+                        readOnly={verifyOnly}
+                        name="recoveryEmail"
+                        id="recoveryEmail"
+                        type="email"
+                        onChange={(e) => setNewRecoveryEmail(e.target.value)}
+                        defaultValue={verifyOnly ? info?.email ?? "" : newRecoveryEmail}
+                        autoComplete="email"
+                    />
+                </div>
+                <div className="mb-2">
+                    <Label>{t('Your current password')}</Label>
+                    <MaskedInput
+                        required
+                        name="pleaesIgnoreMeFirefox"
+                        id="pleaesIgnoreMeFirefox"
+                        type="password"
+                        onChange={(e) => setCurrentPassword(e.target.value)}
+                        defaultValue={currentPassword}
+                        autoComplete="one-time-code"
+                    />
+                </div>
 
-                        <div className="mt-3">
-                            <ActionButton
-                                onClick={() => {
-                                    reset();
-                                    onConfirm();
-                                }}>
-                                {t('Close')}
-                            </ActionButton>
-                        </div>
-                    </div>
-                }
+              {validationError && <div>
+                  <span className="text-red-500">{validationError}</span>
+              </div>}
 
-                {view == 'prompt' &&
-                    <form
-                        onSubmit={(e) => {
-                            e.preventDefault();
-                            e.stopPropagation();
+                <div className="mt-5 flex flex-row-reverse">
+                    <ActionButton state={state}>
+                      {t('Save')}
+                    </ActionButton>
+                </div>
+            </form>}
 
-                            if (e.currentTarget.reportValidity()) {
-                                doUpdateRecoveryEmail();
-                            }
-                        }}
-                    >
-                        <div className="mb-2">
-                            <Label>{t('Your current recovery email')}</Label>
-                            <span>{info?.email}</span>
-                        </div>
-                        <hr className="my-4"/>
-                        <div className="mb-2">
-                            {verifyOnly ?
-                                <Label>{t('Recovery email')}</Label> :
-                                <Label>{t('New Recovery email')}</Label>}
-                            <Input
-                                required
-                                readOnly={verifyOnly}
-                                name="recoveryEmail"
-                                id="recoveryEmail"
-                                type="email"
-                                onChange={(e) => setNewRecoveryEmail(e.target.value)}
-                                defaultValue={newRecoveryEmail}
-                                autoComplete="email"
-                            />
-                        </div>
-                        <div className="mb-2">
-                            <Label>{t('Your current password')}</Label>
-                            <MaskedInput
-                                required
-                                name="pleaesIgnoreMeFirefox"
-                                id="pleaesIgnoreMeFirefox"
-                                type="password"
-                                onChange={(e) => setCurrentPassword(e.target.value)}
-                                defaultValue={currentPassword}
-                                autoComplete="one-time-code"
-                            />
-                        </div>
+      </>
+    </DialogWrapper>
+  );
 
-                        {validationError && <div>
-                            <span className="text-red-500">{validationError}</span>
-                        </div>}
-
-                        <div className="mt-5 flex flex-row-reverse">
-                            <ActionButton state={state}>
-                                {t('Save')}
-                            </ActionButton>
-                        </div>
-                    </form>}
-
-            </>
-        </DialogWrapper>
-    );
-
-    return createPortal(dialog, target);
+  return createPortal(dialog, target);
 };
--- packages/apps/owner-app/src/templates/Security/SecurityOverview.tsx
+++ packages/apps/owner-app/src/templates/Security/SecurityOverview.tsx
@@ -12,189 +12,189 @@ import {Check, Exclamation} from "@homebase-id/common-app/icons";
 
 export const SecurityOverview = () => {
 
-    const [openDialog, setOpenDialog] = useState<'none' | 'verify-password' | 'verify-recovery-phrase' | 'change-email' | 'verify-email'>('none');
-    const [statusLoading, setStatusLoading] = useState(false);
-    const [info, setInfo] = useState<RecoveryInfo | null>();
+  const [openDialog, setOpenDialog] = useState<'none' | 'verify-password' | 'verify-recovery-phrase' | 'change-email' | 'verify-email'>('none');
+  const [statusLoading, setStatusLoading] = useState(false);
+  const [info, setInfo] = useState<RecoveryInfo | null>();
 
-    const reset = async () => {
-        setStatusLoading(true)
-        const status = await getRecoveryInfo();
-        setInfo(status);
-        setStatusLoading(false);
-    }
+  const reset = async () => {
+    setStatusLoading(true)
+    const status = await getRecoveryInfo();
+    setInfo(status);
+    setStatusLoading(false);
+  }
 
-    useEffect(() => {
-        reset();
-    }, []);
+  useEffect(() => {
+    reset();
+  }, []);
 
-    const handleConfirmDialog = async () => {
-        setOpenDialog('none');
-        await reset();
-    }
+  const handleConfirmDialog = async () => {
+    setOpenDialog('none');
+    await reset();
+  }
 
-    console.log(info)
-    return (
-        <>
-            {/*<ErrorNotification error={updateFlagError}/>*/}
+  console.log(info)
+  console.log(openDialog)
+  return (
+    <>
+      {/*<ErrorNotification error={updateFlagError}/>*/}
 
-            <>
-                <Section
-                    title={
-                        <div className="flex flex-col">
-                            {t('Account Recovery Health')}
-                            <small className="text-sm text-gray-400">
-                                {t('')}
-                            </small>
+      <>
+        <Section
+          title={
+            <div className="flex flex-col">
+              {t('Account Recovery Health')}
+              <small className="text-sm text-gray-400">
+                {t('')}
+              </small>
+            </div>
+          }>
+          <>
+            {
+              statusLoading ? (
+                <>
+                  <LoadingBlock className="m-4 h-10"/>
+                  <LoadingBlock className="m-4 h-10"/>
+                  <LoadingBlock className="m-4 h-10"/>
+                  <LoadingBlock className="m-4 h-10"/>
+                </>
+              ) : (
+                <div className="space-y-4">
+                  {/* Recovery Email */}
+                  <RecoveryEmailRow info={info}/>
+
+                  {/* Password Status */}
+                  <div className="flex flex-row flex-wrap items-center">
+                    <div className="flex items-center">
+                      <p>{t('Password last verified:')}</p>
+                      <TimeAgoUtc className="ml-2 text-sm font-medium" value={info?.status?.passwordLastVerified ?? 0}/>
+                    </div>
+                    <Link to={""} className="ml-3 underline" onClick={() => setOpenDialog('verify-password')}>
+                      Verify now
+                    </Link>
+                  </div>
+
+                  {/* Recovery Phrase */}
+                  <div className="flex flex-row flex-wrap items-center">
+                    <div className="flex items-center">
+                      <p>{t('Recovery phrase last verified:')}</p>
+                      <TimeAgoUtc className="ml-2 text-sm font-medium"
+                                  value={info?.status?.recoveryKeyLastVerified ?? 0}/>
+                    </div>
+                    <Link to={""} className="ml-3 underline" onClick={() => setOpenDialog('verify-recovery-phrase')}>
+                      Verify now
+                    </Link>
+                  </div>
+
+                  {/* Password Recovery */}
+                  <div>
+                    <p className="">{t("Trusted Connects Recovery Status")}</p>
+                    {!info?.isConfigured &&
+                        <div className="mt-1">
+                          {/*<span className="mr-3">💀 Recovery not possible.</span>*/}
+                            <Link className="underline" to="/owner/security/password-recovery">
+                                Setup Now
+                            </Link>
                         </div>
-                    }>
-                    <>
-                        {
-                            statusLoading ? (
-                                <>
-                                    <LoadingBlock className="m-4 h-10"/>
-                                    <LoadingBlock className="m-4 h-10"/>
-                                    <LoadingBlock className="m-4 h-10"/>
-                                    <LoadingBlock className="m-4 h-10"/>
-                                </>
-                            ) : (
-                                <div className="space-y-4">
-                                    {/* Recovery Email */}
-                                    <RecoveryEmailRow info={info}/>
 
-                                    {/* Password Status */}
-                                    <div className="flex flex-row flex-wrap items-center">
-                                        <div className="flex items-center">
-                                            <p>{t('Password last verified:')}</p>
-                                            <TimeAgoUtc className="ml-2 text-sm font-medium" value={info?.status?.passwordLastVerified ?? 0}/>
-                                        </div>
-                                        <Link to={""} className="ml-3 underline" onClick={() => setOpenDialog('verify-password')}>
-                                            Verify now
-                                        </Link>
-                                    </div>
+                    }
 
-                                    {/* Recovery Phrase */}
-                                    <div className="flex flex-row flex-wrap items-center">
-                                        <div className="flex items-center">
-                                            <p>{t('Recovery phrase last verified:')}</p>
-                                            <TimeAgoUtc className="ml-2 text-sm font-medium"
-                                                        value={info?.status?.recoveryKeyLastVerified ?? 0}/>
-                                        </div>
-                                        <Link to={""} className="ml-3 underline" onClick={() => setOpenDialog('verify-recovery-phrase')}>
-                                            Verify now
-                                        </Link>
-                                    </div>
+                    {info?.recoveryRisk && (
+                      <div className="p-2 mt-2 space-y-2">
 
-                                    {/* Password Recovery */}
-                                    <div>
-                                        <p className="">{t("Trusted Connects Recovery Status")}</p>
-                                        {!info?.isConfigured &&
-                                            <div className="mt-1">
-                                                {/*<span className="mr-3">💀 Recovery not possible.</span>*/}
-                                                <Link className="underline" to="/owner/security/password-recovery">
-                                                    Setup Now
-                                                </Link>
-                                            </div>
+                        <div className="flex flex-row">
+                          <Label>{t('Last Updated:')}</Label>
+                          <TimeAgoUtc className="ml-2" value={info.recoveryRisk.healthLastChecked ?? 0}/>
+                        </div>
 
-                                        }
+                        <DealerRecoveryRiskHeadline report={info.recoveryRisk}/>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              )}
+          </>
 
-                                        {info?.recoveryRisk && (
-                                            <div className="p-2 mt-2 space-y-2">
+        </Section>
 
-                                                <div className="flex flex-row">
-                                                    <Label>{t('Last Updated:')}</Label>
-                                                    <TimeAgoUtc className="ml-2" value={info.recoveryRisk.healthLastChecked ?? 0}/>
-                                                </div>
+        {/*do this later*/}
+        {/*<Section*/}
+        {/*  title={*/}
+        {/*    <div className="flex flex-col">*/}
+        {/*      {t('Login history')}*/}
+        {/*      <small className="text-sm text-gray-400">*/}
+        {/*        {t('Logins across apps')}*/}
+        {/*      </small>*/}
+        {/*    </div>*/}
+        {/*  }>*/}
+        {/*  <div></div>*/}
+        {/*</Section>*/}
+      </>
 
-                                                <DealerRecoveryRiskHeadline report={info.recoveryRisk}/>
-                                            </div>
-                                        )}
-                                    </div>
-                                </div>
-                            )}
-                    </>
+      {info && <ChangeRecoveryEmailDialog title={t('Change Recovery Email')}
+                                          info={info}
+                                          isOpen={openDialog === 'change-email' || openDialog === 'verify-email'}
+                                          verifyOnly={openDialog === 'verify-email'}
+                                          onConfirm={handleConfirmDialog}
+                                          onCancel={() => setOpenDialog('none')}/>}
 
-                </Section>
-
-                {/*do this later*/}
-                {/*<Section*/}
-                {/*  title={*/}
-                {/*    <div className="flex flex-col">*/}
-                {/*      {t('Login history')}*/}
-                {/*      <small className="text-sm text-gray-400">*/}
-                {/*        {t('Logins across apps')}*/}
-                {/*      </small>*/}
-                {/*    </div>*/}
-                {/*  }>*/}
-                {/*  <div></div>*/}
-                {/*</Section>*/}
-            </>
-
-            <ChangeRecoveryEmailDialog title={t('Change Recovery Email')}
-                                       isOpen={openDialog === 'change-email' || openDialog === 'verify-email'}
-                                       verifyOnly={openDialog === 'verify-email'}
-                                       defaultEmail={info?.email ?? ""}
-                                       onConfirm={handleConfirmDialog}
-                                       onCancel={() => setOpenDialog('none')}/>
-
-            <VerifyPasswordDialog title={t('Verify Password')}
-                                  isOpen={openDialog === 'verify-password'}
-                                  onConfirm={handleConfirmDialog}
-                                  onCancel={() => setOpenDialog('none')}/>
+      <VerifyPasswordDialog title={t('Verify Password')}
+                            isOpen={openDialog === 'verify-password'}
+                            onConfirm={handleConfirmDialog}
+                            onCancel={() => setOpenDialog('none')}/>
 
 
-            <VerifyRecoveryKeyDialog title={t('Verify Recovery Phrase')}
-                                     isOpen={openDialog === 'verify-recovery-phrase'}
-                                     onConfirm={handleConfirmDialog}
-                                     onCancel={() => setOpenDialog('none')}/>
+      <VerifyRecoveryKeyDialog title={t('Verify Recovery Phrase')}
+                               isOpen={openDialog === 'verify-recovery-phrase'}
+                               onConfirm={handleConfirmDialog}
+                               onCancel={() => setOpenDialog('none')}/>
 
 
-        </>
-    );
+    </>
+  );
 
 
-    function RecoveryEmailRow({info}: { info?: RecoveryInfo | null; }) {
-        const isVerified = !!info?.emailLastVerified;
+  function RecoveryEmailRow({info}: { info?: RecoveryInfo | null; }) {
+    const isVerified = !!info?.emailLastVerified;
 
-        return (
-            <div className="flex flex-wrap items-center gap-2">
-                <div className="flex items-center">
-                    <p className="font-medium">{t("Recovery Email:")}</p>
-                    <span className="ml-2 text-zinc-700">{info?.email}</span>
+    return (
+      <div className="flex flex-wrap items-center gap-2">
+        <div className="flex items-center">
+          <p className="font-medium">{t("Recovery Email:")}</p>
+          <span className="ml-2 text-zinc-700">{info?.email}</span>
 
-                    {isVerified ? (
-                        <span className="ml-2 flex items-center text-green-600 text-sm">
+          {isVerified ? (
+            <span className="ml-2 flex items-center text-green-600 text-sm">
             <Check className="h-5 w-5 mr-1" aria-hidden="true"/>
-                            {t("Verified")}
-                            <TimeAgoUtc
-                                className="ml-1 text-sm font-medium"
-                                value={info?.emailLastVerified ?? 0}
-                            />
+              {t("Verified")}
+              <TimeAgoUtc
+                className="ml-1 text-sm font-medium"
+                value={info?.emailLastVerified ?? 0}
+              />
           </span>
-                    ) : (
-                        <span className="ml-2 flex items-center text-red-600 text-sm">
+          ) : (
+            <span className="ml-2 flex items-center text-red-600 text-sm">
             <Exclamation className="h-5 w-5 mr-1" aria-hidden="true"/>
-                            {t("Not Verified")}
-                            <button
-                                type="button"
-                                onClick={() => setOpenDialog("verify-email")}
-                                className="ml-2 underline text-blue-600 hover:text-blue-800"
-                            >
+              {t("Not Verified")}
+              <button
+                type="button"
+                onClick={() => setOpenDialog("verify-email")}
+                className="ml-2 underline text-blue-600 hover:text-blue-800">
               {t("Verify")}
             </button>
           </span>
-                    )}
-                </div>
+          )}
+        </div>
 
-                <button
-                    type="button"
-                    onClick={() => setOpenDialog("change-email")}
-                    className="ml-3 underline text-blue-600 hover:text-blue-800"
-                >
-                    {t("Change")}
-                </button>
-            </div>
-        );
-    }
+        <button
+          type="button"
+          onClick={() => setOpenDialog("change-email")}
+          className="ml-3 underline text-blue-600 hover:text-blue-800"
+        >
+          {t("Change")}
+        </button>
+      </div>
+    );
+  }
 
 
 }
