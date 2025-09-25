50028849586165281fcbb83cdda9ffc36b19d66a
toddmitchell <t@lighttheory.co>
2025-09-25 11:45:33 -0500

    ignoring any for specific case

--- packages/apps/owner-app/src/templates/AccountRecovery/Shamir/ShamirAccountRecovery.tsx
+++ packages/apps/owner-app/src/templates/AccountRecovery/Shamir/ShamirAccountRecovery.tsx
@@ -34,8 +34,9 @@ const ShamirAccountRecovery = () => {
       setErrorMessage(null); // clear previous
       await initiateRecoveryMode();
       await reloadStatus();
-    } catch (err: any) {
-      setErrorMessage(err.message || t("An unexpected error occurred."));
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      setErrorMessage(error.message || t("An unexpected error occurred."));
     }
   };
 
